222ffb829621244552f80e286e5be4e8c0b76ef2
sdardew <sdardew@gmail.com>
2022-05-09 15:20:18 +0900

    feat: JpaVendorAdapter 설정

--- src/main/java/com/prgrms/springbootjpa/config/DataSourceConfig.java
+++ src/main/java/com/prgrms/springbootjpa/config/DataSourceConfig.java
@@ -1,8 +1,12 @@
 package com.prgrms.springbootjpa.config;
 
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
 
@@ -19,5 +23,15 @@ public class DataSourceConfig {
     dataSource.setPassword("");
 
     return dataSource;
+  }
+
+  @Bean
+  public JpaVendorAdapter jpaVendorAdapter(JpaProperties jpaProperties) {
+    // Hibernate 구현체를 사용하기 위한 설정
+    AbstractJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+    jpaVendorAdapter.setShowSql(jpaProperties.isShowSql());
+    jpaVendorAdapter.setDatabasePlatform(jpaProperties.getDatabasePlatform());
+    jpaVendorAdapter.setGenerateDdl(jpaProperties.isGenerateDdl());
+    return jpaVendorAdapter;
   }
 }
