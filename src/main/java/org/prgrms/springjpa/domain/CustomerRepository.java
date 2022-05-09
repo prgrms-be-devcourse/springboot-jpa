1624325d841028c78c0075d5e3170ab1dd168f4c
dbslzld15 <dbslzld15@naver.com>
2022-05-09 17:23:12 +0900

    feat: 고객 엔티티 및 레포 구현

---
+++ src/main/java/org/prgrms/springjpa/domain/CustomerRepository.java
@@ -0,0 +1,6 @@
+package org.prgrms.springjpa.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
