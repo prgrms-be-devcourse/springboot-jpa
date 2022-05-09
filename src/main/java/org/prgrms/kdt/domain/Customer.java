8a83855decb1f7ffeacac61cba0a1fe66c2b324b
midasworld <midas117@knou.ac.kr>
2022-05-09 23:40:24 +0900

    feat: Customer - Entity, Repository 생성 및 테스트

---
+++ src/main/java/org/prgrms/kdt/domain/Customer.java
@@ -0,0 +1,39 @@
+package org.prgrms.kdt.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void changeFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void changeLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
