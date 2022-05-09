011ceeea042fdb217735f6cfd44043c8b2d610f7
kimziou77 <kimziou77@naver.com>
2022-05-09 22:22:25 +0900

    (Refac) Builder 어노테이션 위치 변경
    참고링크 : https://johngrib.github.io/wiki/pattern/builder/

--- jpa2/src/main/java/com/part4/jpa2/domain/Customer.java
+++ jpa2/src/main/java/com/part4/jpa2/domain/Customer.java
@@ -8,12 +8,18 @@ import javax.persistence.Id;
 
 @Entity
 @Data
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class Customer {
     @Id @GeneratedValue
     private long id;
     private String firstName;
     private String lastName;
+
+    @Builder
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
+
