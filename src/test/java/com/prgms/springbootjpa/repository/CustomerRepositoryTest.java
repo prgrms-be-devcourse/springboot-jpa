cfc438998c5ae0fb68690b156613086f53c606eb
hyuk0309 <bahngeh@naver.com>
2022-05-09 17:30:42 +0900

    feat(repository) : jpa 이용 customer CRUD test 작성

---
+++ src/test/java/com/prgms/springbootjpa/repository/CustomerRepositoryTest.java
@@ -0,0 +1,103 @@
+package com.prgms.springbootjpa.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.prgms.springbootjpa.domain.Customer;
+import com.prgms.springbootjpa.domain.Name;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    private Customer customer;
+
+    @BeforeEach
+    void setUp() {
+        customer = new Customer(1L, new Name("eunhyuk", "bahng"));
+    }
+
+    @Test
+    @DisplayName("고객정보 저장")
+    void testSaveSuccess() {
+        //Given
+        //When
+        customerRepository.save(customer);
+
+        //Then
+        var selectedEntity = customerRepository.findById(1L).get();
+        assertAll(
+            () -> assertThat(selectedEntity).isNotNull(),
+            () -> assertThat(selectedEntity.getId()).isEqualTo(1L),
+            () -> assertThat(selectedEntity.getName()).isEqualTo(customer.getName())
+        );
+    }
+
+    @Test
+    @DisplayName("고객정보 수정")
+    void testUpdateSuccess() {
+        //Given
+        var entity = customerRepository.save(customer);
+
+        //When
+        entity.changeName(new Name("honggu", "kang"));
+
+        //Then
+        var selectedEntity = customerRepository.findById(1L).get();
+        assertAll(
+            () -> assertThat(selectedEntity).isNotNull(),
+            () -> assertThat(selectedEntity.getId()).isEqualTo(1L),
+            () -> assertThat(selectedEntity.getName()).isEqualTo(entity.getName())
+        );
+    }
+
+    @Test
+    @DisplayName("단건 조회")
+    void testSelectOneSuccess() {
+        //Given
+        customerRepository.save(customer);
+
+        //When
+        var selected = customerRepository.findById(customer.getId()).get();
+
+        //Then
+        assertThat(selected.getId()).isEqualTo(customer.getId());
+    }
+
+    @Test
+    @DisplayName("다건 조회")
+    void testSelectManySuccess() {
+        //Given
+        var customer2 = new Customer(2L, new Name("honggu", "kang"));
+        customerRepository.saveAll(Lists.newArrayList(customer, customer2));
+
+        //When
+        var selectCustomers = customerRepository.findAll();
+
+        //Then
+        assertThat(selectCustomers.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("고객정보 삭제")
+    void testDeleteSuccess() {
+        //Given
+        customerRepository.save(customer);
+
+        //When
+        customerRepository.deleteById(1L);
+
+        //Then
+        assertThat(customerRepository.findById(1L)).isEmpty();
+    }
+}
