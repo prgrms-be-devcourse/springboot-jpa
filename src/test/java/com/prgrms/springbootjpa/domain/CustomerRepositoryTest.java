7cbefd4dc868450761c85cb1a7ace651873cc4c7
sdardew <sdardew@gmail.com>
2022-05-09 16:16:32 +0900

    test: 고객 엔티티 READ 테스트

--- src/test/java/com/prgrms/springbootjpa/domain/CustomerRepositoryTest.java
+++ src/test/java/com/prgrms/springbootjpa/domain/CustomerRepositoryTest.java
@@ -2,6 +2,8 @@ package com.prgrms.springbootjpa.domain;
 
 import com.prgrms.springbootjpa.repository.CustomerRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,12 +31,27 @@ class CustomerRepositoryTest {
     customer.setLastName("last");
   }
 
+  @AfterEach
+  void afterEach() {
+    repository.deleteAll();
+  }
+
   @Test
   @DisplayName("고객 엔티티 Create 테스트")
   void testCreate() {
     repository.save(customer);
     Optional<Customer> found = repository.findById(1L);
     assertThat(found.isPresent()).isTrue();
+  }
+
+  @Test
+  @DisplayName("고객 엔티티 Read 테스트")
+  void testead() {
+    repository.save(customer);
+    Customer found = repository.findById(1L).get();
+    assertThat(found.getId()).isEqualTo(customer.getId());
+    assertThat(found.getFirstName()).isEqualTo(customer.getFirstName());
+    assertThat(found.getLastName()).isEqualTo(customer.getLastName());
   }
 
   @Test
