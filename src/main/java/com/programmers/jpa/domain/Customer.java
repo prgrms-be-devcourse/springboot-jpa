b5b08f9c7c3a88a595e04e780c3a8d377cd68f22
yanJuicy <hanjub3@gmail.com>
2022-05-09 18:07:57 +0900

    Customer Entity 추가

---
+++ src/main/java/com/programmers/jpa/domain/Customer.java
@@ -0,0 +1,49 @@
+package com.programmers.jpa.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    public Customer() {}
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
