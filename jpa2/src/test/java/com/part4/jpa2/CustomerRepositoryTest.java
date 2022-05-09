50415bc411f420d81fac9230e6d03fa40bcc5669
kimziou77 <kimziou77@naver.com>
2022-05-09 22:23:07 +0900

    (Refac) 중복코드 및 수동 ID 생성 코드 제거

--- jpa2/src/test/java/com/part4/jpa2/CustomerRepositoryTest.java
+++ jpa2/src/test/java/com/part4/jpa2/CustomerRepositoryTest.java
@@ -19,56 +19,40 @@ public class CustomerRepositoryTest {
     private CustomerRepository customerRepository;
 
     Customer customer;
+    Customer savedCustomer;
 
     @BeforeEach
     void 고객_CREATE(){
         customer = Customer.builder()
-                .id(1L)
                 .firstName("subin")
                 .lastName("kim").build();
+        savedCustomer = customerRepository.save(customer);
     }
 
     @Test
     void 고객_READ(){
-        customerRepository.save(customer);
-
-        var entity = customerRepository.findById(1L).get();
+        var entity = customerRepository.findById(savedCustomer.getId()).get();
         assertThat(entity).usingRecursiveComparison().isEqualTo(customer);
     }
 
     @Test
     void 고객_UPDATE_AND_READ(){
-        var customer = Customer.builder()
-                .id(1L)
-                .firstName("subin")
-                .lastName("kim").build();
-        customerRepository.save(customer);
-        var entity = customerRepository.findById(1L).get();
+        var entity = customerRepository.findById(savedCustomer.getId()).get();
 
         entity.setFirstName("SUBIN");
         entity.setLastName("KIM");
 
-        var entity2 = customerRepository.findById(1L).get();
+        var entity2 = customerRepository.findById(savedCustomer.getId()).get();
         assertThat(entity).usingRecursiveComparison().isEqualTo(entity2);
     }
 
     @Test
     void 고객_DELETE_AND_READ(){
-        var customer = Customer.builder()
-                .id(1L)
-                .firstName("subin")
-                .lastName("kim").build();
-        customerRepository.save(customer);
-
         assertThat(customerRepository.count()).isEqualTo(1);
-
-        customerRepository.deleteById(1L);
+        customerRepository.deleteById(savedCustomer.getId());
 
         var allCustomers = customerRepository.findAll();
         assertThat(customerRepository.count()).isEqualTo(0);
         assertThat(allCustomers).doesNotContain(customer);
     }
-
-
-
 }
