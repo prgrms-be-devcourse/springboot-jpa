0edf28eaef916ec8484e5c9b0be5ef1d4e88c60a
Yongsu Kang <dksktm15@naver.com>
2022-05-09 23:22:20 +0900

    테스트 코드 수정

--- src/test/java/com/prgms/mission_jpa/customer/CustomerRepositoryTest.java
+++ src/test/java/com/prgms/mission_jpa/customer/CustomerRepositoryTest.java
@@ -1,15 +1,11 @@
 package com.prgms.mission_jpa.customer;
 
-import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,21 +13,11 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
-@Slf4j
-@SpringBootTest
+@DataJpaTest
 class CustomerRepositoryTest {
 
     @Autowired
     CustomerRepository customerRepository;
-
-    private List<Customer> customers = new ArrayList<>();
-
-    @BeforeEach
-    void setup(){
-        for (long i = 0; i < 10; i++) {
-            customers.add(Customer.builder().firstName("yongsu_" + i).lastName("kang_" + i).build());
-        }
-    }
 
     @AfterEach
     void cleanup(){
@@ -49,12 +35,10 @@ class CustomerRepositoryTest {
         //when
         Customer savedCustomer = customerRepository.save(customer);
         //then
-        assertThat(savedCustomer.getFirstName()).isEqualTo(customer.getFirstName());
         assertThat(savedCustomer).usingRecursiveComparison().isEqualTo(customer);
     }
 
     @Test
-    @Transactional
     void 고객정보를_수정할_수_있다() {
         //given
         Customer customer = Customer.builder()
@@ -67,8 +51,7 @@ class CustomerRepositoryTest {
         savedCustomer.setLastName("kang");
         //then
         Customer updatedCustomer = customerRepository.findById(savedCustomer.getId()).get();
-        assertThat(updatedCustomer.getFirstName()).isEqualTo("yongsu");
-        assertThat(updatedCustomer.getLastName()).isEqualTo("kang");
+        assertThat(updatedCustomer).usingRecursiveComparison().isEqualTo(savedCustomer);
     }
 
     @Test
@@ -83,19 +66,27 @@ class CustomerRepositoryTest {
         customerRepository.deleteById(savedCustomer.getId());
         //then
         Optional<Customer> findCustomer = customerRepository.findById(savedCustomer.getId());
-        assertThat(findCustomer.isEmpty()).isTrue();
+        assertThat(findCustomer).isEmpty();
     }
 
     @Test
     void 전체고객을_삭제할_수_있다(){
         //given
-        customerRepository.saveAll(customers);
+        customerRepository.saveAll(customers());
         List<Customer> findAllCustomers = customerRepository.findAll();
-        assertThat(findAllCustomers.size()).isEqualTo(10);
+        assertThat(findAllCustomers).hasSize(10);
         //when
         customerRepository.deleteAll();
         //then
         List<Customer> afterDeleteCustomers = customerRepository.findAll();
-        assertThat(afterDeleteCustomers.isEmpty()).isTrue();
+        assertThat(afterDeleteCustomers).isEmpty();
+    }
+
+    private List<Customer> customers(){
+        List<Customer> customers = new ArrayList<>();
+        for (long i = 0; i < 10; i++) {
+            customers.add(Customer.builder().firstName("yongsu_" + i).lastName("kang_" + i).build());
+        }
+        return customers;
     }
 }
