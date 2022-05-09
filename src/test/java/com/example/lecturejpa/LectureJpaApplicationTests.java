a56a55251ddd1e1d47707afc948a977c6b79f2f9
pjh612 <pjhjh612@gmail.com>
2022-05-09 19:29:40 +0900

    feat : 고객 Repository CRUD

---
+++ src/test/java/com/example/lecturejpa/LectureJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecturejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
