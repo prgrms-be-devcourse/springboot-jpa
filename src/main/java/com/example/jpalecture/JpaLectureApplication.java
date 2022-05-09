7cd64820f56561ae9fe195341701fa6d010297ff
Hyunji <rioreo2022@gmail.com>
2022-05-09 15:39:25 +0900

    add: 프로젝트 세팅

---
+++ src/main/java/com/example/jpalecture/JpaLectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpalecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaLectureApplication.class, args);
+    }
+
+}
