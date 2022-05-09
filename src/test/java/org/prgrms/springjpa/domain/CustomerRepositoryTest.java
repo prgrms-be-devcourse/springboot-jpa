8cc8026c5501d3ac5e7a9c309fe0d99fa942d93e
dbslzld15 <dbslzld15@naver.com>
2022-05-09 22:29:47 +0900

    fix: ExtendWith 어노테이션 제거

--- src/test/java/org/prgrms/springjpa/domain/CustomerRepositoryTest.java
+++ src/test/java/org/prgrms/springjpa/domain/CustomerRepositoryTest.java
@@ -2,10 +2,8 @@ package org.prgrms.springjpa.domain;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,7 +11,6 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@ExtendWith(SpringExtension.class)
 class CustomerRepositoryTest {
 
     @Autowired
