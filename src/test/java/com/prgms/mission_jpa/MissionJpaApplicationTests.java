dade8e2daf25d803674c0ff8309d25f921404e15
Yongsu Kang <dksktm15@naver.com>
2022-05-09 20:28:58 +0900

    프로젝트 생성

---
+++ src/test/java/com/prgms/mission_jpa/MissionJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgms.mission_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
