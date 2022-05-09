45b0d82f03de4c485c9ff4ace971098c5f77ccba
Yongsu Kang <dksktm15@naver.com>
2022-05-09 22:22:33 +0900

    Customer @Builder, @GeneratedValue 어노테이션 추가

--- src/main/java/com/prgms/mission_jpa/customer/Customer.java
+++ src/main/java/com/prgms/mission_jpa/customer/Customer.java
@@ -3,17 +3,20 @@ package com.prgms.mission_jpa.customer;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "customers")
 public class Customer {
     @Id
+    @GeneratedValue
     private Long id;
     private String firstName;
     private String lastName;
