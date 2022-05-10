2cac7735fc37f990dbc05622ac19397ee0cd0f88
ParkSang <hooo0503@gmail.com>
2022-05-10 13:27:00 +0900

    feat: Customer 도메인 및 repository, test 생성

---
+++ src/main/java/com/example/springbootjpa/domain/Customer.java
@@ -0,0 +1,41 @@
+package com.example.springbootjpa.domain;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Entity
+public class Customer {
+    @Id
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
