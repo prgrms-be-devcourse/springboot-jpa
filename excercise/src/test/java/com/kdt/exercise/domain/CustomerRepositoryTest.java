f82b4298c9fd51dafb02ff1e1f6a8becc29dda78
dhkstnaos <dhkstnaos@gmail.com>
2022-05-10 20:30:10 +0900

    beforeEach 추가

--- excercise/src/test/java/com/kdt/exercise/domain/CustomerRepositoryTest.java
+++ excercise/src/test/java/com/kdt/exercise/domain/CustomerRepositoryTest.java
@@ -2,6 +2,7 @@ package com.kdt.exercise.domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,6 +12,12 @@ class CustomerRepositoryTest {
     @Autowired
     CustomerRepository repository;
 
+    Customer customer;
+    @BeforeEach
+    void setUp() {
+        customer = new Customer("kang", "wansu");
+        repository.save(customer);
+    }
     @AfterEach
     void tearDown() {
         repository.deleteAll();
@@ -18,10 +25,6 @@ class CustomerRepositoryTest {
 
     @Test
     void customer_저장() {
-        //given
-        Customer customer = new Customer("kang", "wansu");
-        //when
-        repository.save(customer);
         //then
         Customer entity = repository.findAll().get(0);
         Assertions.assertThat(entity.getFirstName()).isEqualTo(customer.getFirstName());
@@ -29,10 +32,6 @@ class CustomerRepositoryTest {
 
     @Test
     void customer_수정() {
-        //given
-        Customer customer = new Customer("kang", "wansu");
-        //when
-        repository.save(customer);
         //then
         Customer entity = repository.findAll().get(0);
         //@DynamicUpdate로 인해 바뀐 필드만 수정한다. update customers set first_name=? where id=?
