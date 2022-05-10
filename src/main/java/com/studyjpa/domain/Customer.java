2af04d958b83ec21d1b5ac9a191adb47948a87ef
송무송 (Kate) <real.purple.hae.s@gmail.com>
2022-05-11 04:03:48 +0900

    :sparkles: feat: Customer JPA Repository CRUD 실습해보기

--- src/main/java/com/studyjpa/domain/Customer.java
+++ src/main/java/com/studyjpa/domain/Customer.java
@@ -1,6 +1,8 @@
 package com.studyjpa.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -9,6 +11,8 @@ import javax.persistence.Table;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "customers")
 public class Customer {
