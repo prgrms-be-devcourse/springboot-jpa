2c61c39c359941a00eb3164b6907462e426154c1
Yoonoh <xkakak142@naver.com>
2022-05-10 16:11:17 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/example/assignment1/Assignment1Application.java
@@ -0,0 +1,13 @@
+package com.example.assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assignment1/Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
