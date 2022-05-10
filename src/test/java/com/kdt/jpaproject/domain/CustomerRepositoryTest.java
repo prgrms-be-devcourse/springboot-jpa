0e71a749aed665ed877527c8e46a400b9b8b991d
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-05-10 18:10:45 +0900

    feat: Customer CRUD 테스트 코드 작성

---
+++ src/test/java/com/kdt/jpaproject/domain/CustomerRepositoryTest.java
@@ -0,0 +1,90 @@
+package com.kdt.jpaproject.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
+@SpringBootTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    @AfterEach
+    void clear() {
+        repository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Customer 를 create 한다.")
+    public void create() throws Exception {
+        // given
+        Customer customer = Customer.builder()
+                .id(1L)
+                .firstName("Lee")
+                .lastName("YongHoon")
+                .build();
+
+
+        // when
+        repository.save(customer);
+
+        // then
+        Customer entity = repository.findById(1L).get();
+        assertEquals(entity.getId(), 1L);
+        assertEquals(entity.getFirstName(), "Lee");
+        assertEquals(entity.getLastName(), "YongHoon");
+        assertNotEquals(entity, customer); // 동일성 보장X
+    }
+
+    @Test
+    @DisplayName("Customer 를 update 한다.")
+    public void update() throws Exception {
+        // given
+        Customer customer = Customer.builder()
+                .id(1L)
+                .firstName("Lee")
+                .lastName("YongHoon")
+                .build();
+
+        // when
+        repository.save(customer);
+        Customer findCustomer = repository.findById(1L).get();
+        findCustomer.changeFirstName("Kim");
+        findCustomer.changeLastName("KKang");
+        repository.save(findCustomer);
+
+        // then
+
+        Customer entity = repository.findById(1L).get();
+        assertEquals(entity.getId(), 1L);
+        assertEquals(entity.getLastName(), "KKang");
+        assertEquals(entity.getFirstName(), "Kim");
+        assertNotEquals(entity, findCustomer); // 동일성 보장X
+    }
+
+    @Test
+    @DisplayName("Customer 를 delete 한다.")
+    public void delete() throws Exception {
+        // given
+        Customer customer = Customer.builder()
+                .id(1L)
+                .firstName("Lee")
+                .lastName("YongHoon")
+                .build();
+
+        // when
+        repository.save(customer);
+        repository.deleteById(1L);
+
+        // then
+        long size = repository.count();
+        assertEquals(size, 0);
+    }
+}
