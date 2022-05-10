b6a668c3c2a208cc81ff513a52f041ad81cc577a
sdardew <sdardew@gmail.com>
2022-05-10 17:34:37 +0900

    Customer 데이터 저장

--- src/test/java/com/prgrms/springbootjpa/domain/CustomerPersistenceContextTest.java
+++ src/test/java/com/prgrms/springbootjpa/domain/CustomerPersistenceContextTest.java
@@ -3,10 +3,14 @@ package com.prgrms.springbootjpa.domain;
 import com.prgrms.springbootjpa.repository.CustomerRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 
 @Slf4j
 @SpringBootTest
@@ -23,4 +27,20 @@ public class CustomerPersistenceContextTest {
     repository.deleteAll();
   }
 
+  @Test
+  @DisplayName("Customer 데이터 저장")
+  void testCreateCustomer() {
+    EntityManager entityManager = emf.createEntityManager();
+    EntityTransaction transaction = entityManager.getTransaction();
+
+    transaction.begin();
+
+    Customer customer = new Customer();
+    customer.setId(1L);
+    customer.setFirstName("first");
+    customer.setLastName("last");
+
+    entityManager.persist(customer);
+    transaction.commit();
+  }
 }
