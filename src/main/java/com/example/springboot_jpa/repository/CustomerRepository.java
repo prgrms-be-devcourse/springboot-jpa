ef09b52bd7c4145a9495eeec8274a4da8ff8983b
Minsung Kim <epicblue@hanmail.net>
2022-05-10 21:33:55 +0900

    영속성 테스트

--- src/main/java/com/example/springboot_jpa/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboot_jpa.repository;
-
-import com.example.springboot_jpa.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
