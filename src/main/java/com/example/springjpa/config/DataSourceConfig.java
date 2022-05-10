26ea751fc96040542d4c0fc220634eb41f153b7a
hymn-fly <rlfrmsdh1@gmail.com>
2022-05-10 20:14:52 +0900

    수정  - h2 url 설정 변경

--- src/main/java/com/example/springjpa/config/DataSourceConfig.java
+++ src/main/java/com/example/springjpa/config/DataSourceConfig.java
@@ -22,7 +22,7 @@ public class DataSourceConfig {
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:~/test");
+        dataSource.setUrl("jdbc:h2:mem:test;DATABASE_TO_UPPER=false;DB_CLOSE_DELAY=-1");
         dataSource.setUsername("sa");
         dataSource.setPassword("");
         return dataSource;
