884d3c51983a780bd0f448eb4eb492549b7fd601
yerimmac <yerim.lee.earth@gmail.com>
2022-05-10 19:26:53 +0900

    feat : JPA 프로젝트를 세팅한다

---
+++ src/main/java/com/example/jpamission/JpaMissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpamission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaMissionApplication.class, args);
+	}
+
+}
