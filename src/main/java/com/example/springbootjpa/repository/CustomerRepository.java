dfc2249ee80f3d41bd39ac0379d5f7536b7d1840
ParkSang <hooo0503@gmail.com>
2022-05-10 19:33:23 +0900

    restore: 삭제한 Customer 관련 Class들 원복

---
+++ src/main/java/com/example/springbootjpa/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.example.springbootjpa.repository;
+
+import com.example.springbootjpa.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
