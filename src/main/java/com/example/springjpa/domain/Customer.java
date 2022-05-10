d4b2e7fcbb000db34bc7a56e857a474d8081ec21
hymn-fly <rlfrmsdh1@gmail.com>
2022-05-10 20:26:15 +0900

    수정. Customer  - @Getter, @Setter  - @GeneratedValue

--- src/main/java/com/example/springjpa/domain/Customer.java
+++ src/main/java/com/example/springjpa/domain/Customer.java
@@ -1,41 +1,34 @@
 package com.example.springjpa.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name="customers")
+@Getter
+@Setter
+@ToString
 public class Customer {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     private String firstName;
 
     private String lastName;
 
-    public long getId() {
-        return id;
+    public Customer(){
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-
+    public Customer(String firstName, String lastName){
         this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+
 }
