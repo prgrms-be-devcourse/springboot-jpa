fa828c8a27da01e8f44f695b8f1bb01404fdc80f
ICCHOI <cicblck@gmail.com>
2022-05-12 01:08:25 +0900

    feat:setOrder를 구현하라

--- src/main/java/org/programmers/kdt/studyjpa/domain/order/OrderItem.java
+++ src/main/java/org/programmers/kdt/studyjpa/domain/order/OrderItem.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "order_item")
-public class OrderItem {
+public class OrderItem  {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -25,4 +26,12 @@ public class OrderItem {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id", referencedColumnName = "id")
     private Item item;
+
+    public void setOrder(Order order) {
+        if (Objects.nonNull(this.order)) {
+            order.getOrderItems().remove(this);
+        }
+        this.order = order;
+        order.getOrderItems().add(this);
+    }
 }
