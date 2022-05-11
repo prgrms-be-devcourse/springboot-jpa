ae29e20ac4ab70a2dd70cf26a1d066e370a05315
will kim <78planet@gmail.com>
2022-05-11 18:14:51 +0900

    fix: Add Getter, Setter, GenratedValue and Builder to Customer Class

--- src/main/java/com/will/jpapractice/domain/Customer.java
+++ src/main/java/com/will/jpapractice/domain/Customer.java
@@ -1,36 +1,27 @@
 package com.will.jpapractice.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter @Setter
+@NoArgsConstructor
 public class Customer {
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String firstName;
     private String lastName;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
+    @Builder
+    public Customer(String firstName, String lastName) {
         this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 }
