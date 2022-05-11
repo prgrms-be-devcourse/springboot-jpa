5fea6552cd865fce1f1e825df9b07cd58111101b
Yoonoh <xkakak142@naver.com>
2022-05-11 12:53:32 +0900

    refactor: @SpringBootTest -> @DataJpaTest로 변경

--- src/test/java/com/example/assignment1/domain/CustomerRepositoryTest.java
+++ src/test/java/com/example/assignment1/domain/CustomerRepositoryTest.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
-@SpringBootTest
+@DataJpaTest
 class CustomerRepositoryTest {
 
     @Autowired
