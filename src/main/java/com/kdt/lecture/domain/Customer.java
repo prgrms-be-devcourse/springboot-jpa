151ffd934302688bb7fa3e9d082c184606058edc
ordilov <ordilov@gmail.com>
2022-05-11 12:59:08 +0900

    refactor: customer에 validation을 추가하라

--- src/main/java/com/kdt/lecture/domain/Customer.java
+++ src/main/java/com/kdt/lecture/domain/Customer.java
@@ -1,8 +1,11 @@
 package com.kdt.lecture.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,7 +20,13 @@ public class Customer {
 
   @Id
   private Long id;
+
+  @NotBlank @Size(min = 2, max = 20)
+  @Column(name = "first_name", length = 20, nullable = false)
   private String firstName;
+
+  @NotBlank @Size(min = 2, max = 20)
+  @Column(name = "last_name", length = 20, nullable = false)
   private String lastName;
 
   public void updateFullName(String firstName, String lastName) {
