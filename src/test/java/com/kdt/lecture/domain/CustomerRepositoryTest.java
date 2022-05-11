6ef8c82d615803df6a29c8f5253acee958c74c1f
unnokid <kimki0609@gmail.com>
2022-05-11 18:35:10 +0900

    Add `update_test()` and `delete_test()`

--- src/test/java/com/kdt/lecture/domain/CustomerRepositoryTest.java
+++ src/test/java/com/kdt/lecture/domain/CustomerRepositoryTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Slf4j
 class CustomerRepositoryTest {
@@ -13,7 +17,7 @@ class CustomerRepositoryTest {
     CustomerRepository repository;
 
     @Test
-    void test() {
+    void insert_Test() {
         //given
         Customer customer = new Customer();
         customer.setId(1L);
@@ -26,5 +30,46 @@ class CustomerRepositoryTest {
         //then
         Customer entity = repository.findById(1L).get();
         log.info("{}{}", entity.getFirstName(), entity.getLastName());
+        assertThat(entity.getFirstName()).isEqualTo("kim");
+        assertThat(entity.getLastName()).isEqualTo("gong");
+    }
+
+    @Test
+    void update_Test() {
+        //given
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("kim");
+        customer.setLastName("gong");
+        repository.save(customer);
+
+        Customer entity = repository.findById(1L).get();
+        assertThat(entity.getFirstName()).isEqualTo("kim");
+        assertThat(entity.getLastName()).isEqualTo("gong");
+
+        customer.setFirstName("Lee");
+        repository.save(customer);
+
+        Customer update = repository.findById(1L).get();
+        assertThat(update.getFirstName()).isEqualTo("Lee");
+        assertThat(update.getLastName()).isEqualTo("gong");
+    }
+
+    @Test
+    void delete_Test() {
+        //given
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("kim");
+        customer.setLastName("gong");
+        repository.save(customer);
+
+        Customer entity = repository.findById(1L).get();
+        assertThat(entity.getFirstName()).isEqualTo("kim");
+        assertThat(entity.getLastName()).isEqualTo("gong");
+
+        repository.delete(customer);
+        Optional<Customer> delete_entity = repository.findById(1L);
+        assertThat(delete_entity).isEmpty();
     }
 }
