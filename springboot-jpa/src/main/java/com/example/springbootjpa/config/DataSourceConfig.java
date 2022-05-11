5de72192020219759acb390028d45f699299859f
DESKTOP-5836PEC\tiger <tiger980424@naver.com>
2022-05-12 00:13:26 +0900

    미션1 : JPA 소개(단일 엔티티를 이용한 CRUD를 구현)

---
+++ springboot-jpa/src/main/java/com/example/springbootjpa/config/DataSourceConfig.java
@@ -0,0 +1,72 @@
+package com.example.springbootjpa.config;
+
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.example.springbootjpa.mission1")
+public class DataSourceConfig {
+
+    //어떤 Datasource를 사용할지
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:~/mission");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+
+    //어떤 dialect 구현체를 사용할지 결정
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(JpaProperties jpaProperties) {
+        AbstractJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setShowSql(jpaProperties.isShowSql());
+        adapter.setDatabasePlatform(jpaProperties.getDatabasePlatform());
+        adapter.setGenerateDdl(jpaProperties.isGenerateDdl());
+        return adapter;
+    }
+
+
+    //Entity 객체를 생성하고 관리해주는 것
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter,
+                                                                       JpaProperties jpaProperties) {
+        LocalContainerEntityManagerFactoryBean em
+                = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.example.springbootjpa.mission1");
+        em.setJpaVendorAdapter(jpaVendorAdapter);
+
+        Properties properties = new Properties();
+        properties.putAll(jpaProperties.getProperties());
+        em.setJpaProperties(properties);
+
+        return em;
+    }
+
+
+    //트랜잭션을 관리하기 위함
+    @Bean
+    public PlatformTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory.getObject());
+
+        return transactionManager;
+    }
+}
---
+++ springboot-jpa/src/test/java/com/example/springbootjpa/mission1/CustomerRepositoryTest.java
@@ -0,0 +1,119 @@
+package com.example.springbootjpa.mission1;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@DataJpaTest
+@Transactional
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @AfterEach
+    void cleanUp(){
+        customerRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Customer Create 테스트")
+    void createCustomer(){
+        List<Customer> customerList = new ArrayList<>();
+        for(long i = 0; i<10; i++) {
+            Customer customer = new Customer();
+            customer.setId(i);
+            customer.setFirstName(MessageFormat.format("User{0}", i));
+            customer.setLastName(MessageFormat.format("Test{0}", i));
+            customerRepository.save(customer);
+            customerList.add(customer);
+        }
+
+        entityManager.flush();
+        entityManager.clear();
+
+        var foundList = customerRepository.findAll();
+
+        assertThat(foundList).usingRecursiveFieldByFieldElementComparator().isEqualTo(customerList);
+    }
+
+    @Test
+    @DisplayName("Customer Read 테스트")
+    void readCustomer(){
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("Ella");
+        customer.setLastName("Ma");
+        customerRepository.save(customer);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        var foundResult = customerRepository.findById(customer.getId());
+
+        assertThat(foundResult.isPresent()).isTrue();
+        assertThat(foundResult.get()).usingRecursiveComparison().isEqualTo(customer);
+    }
+
+    @Test
+    @DisplayName("Customer Update 테스트")
+    void updateCustomer(){
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("Ella");
+        customer.setLastName("Ma");
+        customerRepository.save(customer);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        Customer foundCustomer = customerRepository.findById(customer.getId()).get();
+        foundCustomer.setFirstName("Updated");
+        customerRepository.save(foundCustomer);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        Customer updatedCustomer = customerRepository.findById(foundCustomer.getId()).get();
+
+        assertThat(updatedCustomer).usingRecursiveComparison().isEqualTo(foundCustomer);
+    }
+
+    @Test
+    @DisplayName("Customer Delete 테스트")
+    void deleteCustomer() {
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("Ella");
+        customer.setLastName("Ma");
+        customerRepository.save(customer);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        Customer foundCustomer = customerRepository.findById(customer.getId()).get();
+        customerRepository.delete(foundCustomer);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        var checkDeleteResult = customerRepository.findById(customer.getId());
+
+        assertThat(checkDeleteResult.isEmpty()).isTrue();
+    }
+}
