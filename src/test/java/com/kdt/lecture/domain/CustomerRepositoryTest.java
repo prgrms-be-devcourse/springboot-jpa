5935bee582209c81020d982dea2f3c9ed8089fe2
Hyunggeun447 <bopi447@naver.com>
2022-05-11 16:01:08 +0900

    mission jpa

---
+++ src/test/java/com/kdt/lecture/domain/CustomerRepositoryTest.java
@@ -0,0 +1,106 @@
+package com.kdt.lecture.domain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * CRUD test
+ */
+@Slf4j
+@SpringBootTest
+@Transactional
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository repository;
+
+    @BeforeEach
+    void clear() {
+        repository.deleteAll();
+    }
+
+    private final String FIRST_NAME = "firstName";
+    private final String NEW_FIRST_NAME = "newFirstName";
+
+    @Test
+    public void C() throws Exception {
+
+        //given
+        Customer customer = new Customer(1L, FIRST_NAME, "lastName");
+
+        //when
+        Customer save = repository.save(customer);
+
+        //then
+        assertThat(repository.findAll().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void 같은_Id의_고객_저장하면_같은_고객_취급() throws Exception {
+
+        //given
+        Customer customer = new Customer(1L, FIRST_NAME, "lastName");
+        Customer customer2 = new Customer(1L, NEW_FIRST_NAME, "new lastName");
+
+        //when
+        Customer save = repository.save(customer);
+        repository.save(customer2);
+
+        //then
+        assertThat(repository.findAll().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void R() throws Exception {
+
+        //given
+        Customer customer = new Customer(1L, FIRST_NAME, "lastName");
+        Customer save = repository.save(customer);
+
+        //when
+        Optional<Customer> byId = repository.findById(1L);
+
+        //then
+        assertThat(byId.isPresent()).isTrue();
+        assertThat(byId.get().getFirstName()).isEqualTo(FIRST_NAME);
+        assertThat(byId.get().getLastName()).isEqualTo("lastName");
+    }
+
+    @Test
+    public void U() throws Exception {
+
+        //given
+        Customer customer = new Customer(1L, FIRST_NAME, "lastName");
+        Customer save = repository.save(customer);
+
+        //when
+        save.changeFirstName(NEW_FIRST_NAME);
+
+        //then
+        assertThat(repository.findById(1L).get().getFirstName()).isEqualTo(NEW_FIRST_NAME);
+    }
+
+    @Test
+    public void D() throws Exception {
+
+        //given
+        Customer customer = new Customer(1L, FIRST_NAME, "lastName");
+        Customer save = repository.save(customer);
+        assertThat(repository.findAll().size()).isEqualTo(1);
+
+        //when
+        repository.delete(save);
+
+        //then
+        assertThat(repository.findById(1L).isEmpty()).isTrue();
+        assertThat(repository.findAll().size()).isEqualTo(0);
+    }
+}
---
+++ src/test/java/com/kdt/lecture/domain/CustomerRepositoryTest2.java
@@ -0,0 +1,95 @@
+package com.kdt.lecture.domain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest
+@Transactional
+public class CustomerRepositoryTest2 {
+
+    @Autowired
+    EntityManagerFactory emf;
+
+    private final String FIRST_NAME = "firstName";
+    private final String NEW_FIRST_NAME = "newFirstName";
+
+    @Test
+    public void 영속_조회_및_수정_변경감지() throws Exception {
+
+        //given
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        Customer customer = new Customer(1L, FIRST_NAME, "lastName");
+        em.persist(customer);
+
+        transaction.commit();
+
+        //when
+        customer.changeFirstName(NEW_FIRST_NAME);
+
+        //then
+        Customer entity = em.find(Customer.class, 1L);
+        assertThat(entity.getFirstName()).isEqualTo(NEW_FIRST_NAME); //변경감지
+    }
+
+    @Test
+    public void 준영속_변경감지_실패() throws Exception {
+
+        //given
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        Customer customer = new Customer(2L, FIRST_NAME, "lastName");
+        em.persist(customer);
+
+        transaction.commit();
+
+        //when
+        transaction.begin();
+        em.detach(customer);
+        transaction.commit();
+
+        customer.changeFirstName(NEW_FIRST_NAME);
+
+        //then
+        Customer entity = em.find(Customer.class, 2L);
+        assertThat(entity.getFirstName()).isEqualTo(FIRST_NAME); //변경감지
+    }
+
+    @Test
+    public void 삭제() throws Exception {
+
+        //given
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        Customer customer = new Customer(3L, FIRST_NAME, "lastName");
+        em.persist(customer);
+
+        transaction.commit();
+
+        //when
+        transaction.begin();
+        Customer entity = em.find(Customer.class, 3L);
+        em.remove(entity);
+        transaction.commit();
+
+        //then
+        assertThat(em.find(Customer.class, 3L)).isNull();
+    }
+}
