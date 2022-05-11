7d4b4e20ab1c8a989b8576708ee62e43016f8ef6
sdardew <sdardew@gmail.com>
2022-05-11 23:55:43 +0900

    Order-OrderItem 연관관계 편의 메서드

--- src/main/java/com/prgrms/springbootjpa/domain/Order.java
+++ src/main/java/com/prgrms/springbootjpa/domain/Order.java
@@ -2,6 +2,7 @@ package com.prgrms.springbootjpa.domain;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "order")
@@ -12,6 +13,11 @@ public class Order {
 
   @OneToMany(mappedBy = "order")
   private List<OrderItem> orderItems;
+
+  public void addOrderItem(OrderItem orderItem) {
+    orderItems.add(orderItem);
+    orderItem.setOrder(this);
+  }
 
   public List<OrderItem> getOrderItems() {
     return orderItems;
