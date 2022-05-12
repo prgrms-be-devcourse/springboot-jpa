50e29822c0556b6a66dc718ce6a4c765caa8ec0d
suy2on <tnwjdrms@naver.com>
2022-05-12 15:10:23 +0900

    # feat : JPA쓰기위한 빈등록

---
+++ src/main/java/com/example/springbootjpa/config/DataSourceConfig.java
@@ -0,0 +1,66 @@
+package com.example.springbootjpa.config;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:~/springboot");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(JpaProperties jpaProperties){
+        AbstractJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setShowSql(jpaProperties.isShowSql());
+        jpaVendorAdapter.setDatabasePlatform(jpaProperties.getDatabasePlatform());
+        jpaVendorAdapter.setGenerateDdl(jpaProperties.isGenerateDdl());
+
+        return jpaVendorAdapter;
+    }
+
+
+    // entity manager 생성
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter, JpaProperties jpaProperties){
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.example.springbootjpa.domain");
+        em.setJpaVendorAdapter(jpaVendorAdapter);
+
+        Properties properties = new Properties();
+        properties.putAll(jpaProperties.getProperties());
+        em.setJpaProperties(properties);
+        return em;
+    }
+
+
+    // transaction 관리
+    @Bean
+    public PlatformTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory.getObject());
+
+        return transactionManager;
+    }
+
+}
