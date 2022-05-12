1232bc32442b86b7f7266f6f2f0ab7dba970e394
hayoung <>
2022-05-12 12:30:16 +0900

    test: 중복된 트랜잭션 어노테이션 삭제

--- src/test/java/com/example/kdtjpa/domain/CustomerRepositoryTest.java
+++ src/test/java/com/example/kdtjpa/domain/CustomerRepositoryTest.java
@@ -4,12 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
-@Transactional
 @DataJpaTest
 class CustomerRepositoryTest {
     @Autowired
