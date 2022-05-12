16db03ecba3f99c507c407f92e1af975bf3214e0
unnokid <kimki0609@gmail.com>
2022-05-12 18:52:33 +0900

    Add Order

---
+++ src/main/java/com/kdt/lecture/domain/order/Order.java
@@ -0,0 +1,51 @@
+package com.kdt.lecture.domain.order;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @Column(name = "id")
+    private String uuid;
+    @Column(name = "memo")
+    private String memo;
+    @Enumerated(value = EnumType.STRING)
+    private OrderStatus orderStatus;
+    @Column(name = "order_datetime", columnDefinition = "TIMESTAMP")
+    private LocalDateTime orderDateTime;
+
+    // member_fk
+    @Column(name = "member_id", insertable = false, updatable = false)
+    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", referencedColumnName = "id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems;
+
+    //연관관계 편의 메소드
+    public void setMember(Member member) {
+        if(Objects.nonNull(this.member)) {
+            this.member.getOrders().remove(this);
+        }
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItem.setOrder(this);
+    }
+
+}
