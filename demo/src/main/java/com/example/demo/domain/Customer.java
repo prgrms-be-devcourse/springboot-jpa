aad1a354144dbda3a6d30a8313a1e211340527c2
yerimmac <yerim.lee.earth@gmail.com>
2022-05-12 13:42:38 +0900

    feat : Customer 엔티티를 이용한 CRUD를 구현한다

---
+++ demo/src/main/java/com/example/demo/domain/Customer.java
@@ -0,0 +1,25 @@
+package com.example.demo.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String lastName;
+}
