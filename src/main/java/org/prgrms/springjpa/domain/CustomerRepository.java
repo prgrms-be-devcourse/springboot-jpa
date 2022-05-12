aea92242006cf893ee6711fc1e21e8eb74c42ee8
dbslzld15 <dbslzld15@naver.com>
2022-05-12 23:59:22 +0900

    fix: h2 환경설정 수정

--- src/main/java/org/prgrms/springjpa/domain/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package org.prgrms.springjpa.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
