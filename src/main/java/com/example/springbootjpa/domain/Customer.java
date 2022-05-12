a3cf9a3ab3fd35dc1eb6fdbf74d853aa7543f48d
suy2on <tnwjdrms@naver.com>
2022-05-12 15:11:46 +0900

    # feat : 영속성컨텍스트 라이프사이클 테스트

--- src/main/java/com/example/springbootjpa/domain/Customer.java
+++ src/main/java/com/example/springbootjpa/domain/Customer.java
@@ -4,13 +4,15 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "customer")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
 
     @Id
@@ -20,4 +22,27 @@ public class Customer {
 
     private int age;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
