20e360ab1fe50c940e9f33f3f0ed5f7227dc0725
yerimmac <yerim.lee.earth@gmail.com>
2022-05-12 23:02:28 +0900

    feat : OrderItem과 Item 양방향 연관관계를 매핑한다

--- demo/src/main/java/com/example/demo/domain/Item.java
+++ demo/src/main/java/com/example/demo/domain/Item.java
@@ -1,11 +1,14 @@
 package com.example.demo.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -14,4 +17,17 @@ public class Item {
     private Long price;
     @Column(name = "stock_quantity")
     private int stockQuantity;
+
+    @OneToOne(mappedBy = "item")
+    private OrderItem orderItem;
+
+    public Item(String name, Long price, int stockQuantity) {
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public void setOrderItem(OrderItem orderItem) {
+        this.orderItem = orderItem;
+    }
 }
--- demo/src/main/java/com/example/demo/domain/OrderItem.java
+++ demo/src/main/java/com/example/demo/domain/OrderItem.java
@@ -1,9 +1,15 @@
 package com.example.demo.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "order_item")
 public class OrderItem {
     @Id
@@ -17,8 +23,16 @@ public class OrderItem {
     @JoinColumn(name = "order_id")
     private Order order;
 
-    @Column(name = "item_id") //fk
-    private Long itemId;
+    @OneToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    public OrderItem(int quantity, Order order, Item item) {
+        this.quantity = quantity;
+        this.order = order;
+        this.item = item;
+        this.price = item.getPrice() * quantity;
+    }
 
     public void setOrder(Order order) {
         if(Objects.nonNull(this.order)) {
@@ -26,6 +40,11 @@ public class OrderItem {
         }
         this.order = order;
         order.getOrderItems().add(this);
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+        item.setOrderItem(this);
     }
 
 }
