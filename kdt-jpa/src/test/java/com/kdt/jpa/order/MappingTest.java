3b7033b1cdc9596364dcb1f065e823a450d9bd76
ChoiYeonho0903 <903yh@naver.com>
2022-05-13 23:13:06 +0900

    연관관계 테스트 수정

--- kdt-jpa/src/test/java/com/kdt/jpa/order/MappingTest.java
+++ kdt-jpa/src/test/java/com/kdt/jpa/order/MappingTest.java
@@ -1,5 +1,7 @@
 package com.kdt.jpa.order;
 
+import com.kdt.jpa.order.domain.Food;
+import com.kdt.jpa.order.domain.Item;
 import com.kdt.jpa.order.domain.Member;
 import com.kdt.jpa.order.domain.Order;
 import com.kdt.jpa.order.domain.OrderStatus;
@@ -9,8 +11,11 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.weaver.ast.Or;
 import org.assertj.core.api.Assertions;
 import org.hibernate.LazyInitializationException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,52 +29,29 @@ public class MappingTest {
     @Autowired
     EntityManagerFactory emf;
 
-    Order order;
-    Member member;
-
-    @BeforeEach
-    void setUp() {
+    @Test
+    @DisplayName("양방향 연관관계 테스트")
+    void twoWayAssociation() {
         EntityManager em = emf.createEntityManager();
         EntityTransaction ts = em.getTransaction();
         ts.begin();
 
-        member = new Member();
+        Member member = new Member();
         member.setName("CHOI");
         member.setNickName("C");
         member.setAddress("서울시");
         member.setAge(27);
         em.persist(member);
 
-        order = new Order();
+        Order order = new Order();
         order.setUuid(UUID.randomUUID().toString());
         order.setMemo("memo");
         order.setOrderStatus(OrderStatus.OPEND);
         order.setMember(member);
         em.persist(order);
 
-        ts.commit();
-    }
-
-    @Test
-    @DisplayName("단방향 연관관계")
-    void test1() {
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction ts = em.getTransaction();
-        ts.begin();
-
-        Order findOrder = em.find(Order.class, order.getUuid());
-        Member findMember = findOrder.getMember();
-        log.info("{} {}", findMember.getId(), findMember.getName());
-
-        ts.commit();
-    }
-
-    @Test
-    @DisplayName("양방향 연관관계")
-    void Test2() {
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction ts = em.getTransaction();
-        ts.begin();
+        em.flush();
+        em.clear(); // 영속성 컨텍스트 초기화
 
         Order findOrder = em.find(Order.class, order.getUuid());
         List<Order> orders = findOrder.getMember().getOrders();
@@ -79,22 +61,64 @@ public class MappingTest {
     }
 
     @Test
-    @DisplayName("프록시 객체 생성")
-    void Test3() {
+    @DisplayName("상속 관계 매핑 테스트")
+    void inheritanceMappingTest() {
         EntityManager em = emf.createEntityManager();
         EntityTransaction ts = em.getTransaction();
         ts.begin();
 
-        Member findMember = em.getReference(Member.class, member.getId());
-        System.out.println(findMember.getName());
+        Food food = new Food();
+        food.setPrice(1000);
+        food.setStockQuantity(100);
+        food.setChef("CHOI");
+
+        em.persist(food);
+
+        ts.commit();
+    }
+
+    @Test
+    @DisplayName("프록시 객체 생성")
+    void proxyEntityTest() {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction ts = em.getTransaction();
+        ts.begin();
+
+        Member member = new Member();
+        member.setName("CHOI");
+        member.setNickName("C");
+        member.setAddress("서울시");
+        member.setAge(27);
+        em.persist(member);
+
+        em.flush();
+        em.clear();
+
+        Member findMember = em.getReference(Member.class, member.getId()); // 프록시 객체 조회
+
+        // DB조회를 통해서(SELECT) 실제 Entity 생성
+        findMember.getName();
+
+        ts.commit();
+        em.close();
     }
 
     @Test
     @DisplayName("준영속 상태일 때, 프록시 초기화시 에러발생")
-    void test4() {
+    void proxyEntityTest2() {
         EntityManager em = emf.createEntityManager();
         EntityTransaction ts = em.getTransaction();
         ts.begin();
+
+        Member member = new Member();
+        member.setName("CHOI");
+        member.setNickName("C");
+        member.setAddress("서울시");
+        member.setAge(27);
+        em.persist(member);
+
+        em.flush();
+        em.clear();
 
         Member reference = em.getReference(Member.class, member.getId());
         System.out.println(reference.getClass().getName());
@@ -107,38 +131,60 @@ public class MappingTest {
 
     @Test
     @DisplayName("영속화 전이 성공")
-    void test5() {
+    void persistenceTransitionTest() {
         EntityManager em = emf.createEntityManager();
         EntityTransaction ts = em.getTransaction();
         ts.begin();
 
-        Member newMember = new Member();
-        newMember.setName("CHOI");
-        newMember.setNickName("CC");
-        newMember.setAddress("서울시");
-        newMember.setAge(27);
+        Member member = new Member();
+        member.setName("CHOI");
+        member.setNickName("C");
+        member.setAddress("서울시");
+        member.setAge(27);
+        em.persist(member);
 
-        Order newOrder = new Order();
-        newOrder.setUuid(UUID.randomUUID().toString());
-        newOrder.setMemo("memo");
-        newOrder.setOrderStatus(OrderStatus.OPEND);
-        newOrder.setMember(newMember);
+        Order order = new Order();
+        order.setUuid(UUID.randomUUID().toString());
+        order.setMemo("memo");
+        order.setOrderStatus(OrderStatus.OPEND);
+        order.setMember(member);
+        em.persist(order);
 
-        em.persist(newOrder);
+        // Cascade.All 설정으로 인해 order, member 영속화 전이된다.
+        em.persist(order);
 
         ts.commit();
     }
 
     @Test
-    @DisplayName("고아 객체 제거")
-    public void test6() {
+    @DisplayName("고아 객체 제거 테스트")
+    public void orphanRemovalTest() {
         EntityManager em = emf.createEntityManager();
         EntityTransaction ts = em.getTransaction();
         ts.begin();
 
-        Member findMember = em.find(Member.class, this.member.getId());
+        Member member = new Member();
+        member.setName("CHOI");
+        member.setNickName("C");
+        member.setAddress("서울시");
+        member.setAge(27);
+        em.persist(member);
+
+        Order order = new Order();
+        order.setUuid(UUID.randomUUID().toString());
+        order.setMemo("memo");
+        order.setOrderStatus(OrderStatus.OPEND);
+        order.setMember(member);
+        em.persist(order);
+
+        em.flush();
+        em.clear();
+
+        Member findMember = em.find(Member.class, member.getId());
+
+        // order, member 모두 삭제 (orphanRemoval = true)
         em.remove(findMember);
-        // order, member 모두 삭제
+
         ts.commit();
     }
 }
