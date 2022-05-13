5e12f2db15cc5d870c52ce24aead6649c74de39c
dbslzld15 <dbslzld15@naver.com>
2022-05-13 10:37:46 +0900

    fix: BaseEntity 추상클래스로 변경, updatable 추가

--- src/main/java/org/prgrms/springjpa/domain/order/BaseEntity.java
+++ src/main/java/org/prgrms/springjpa/domain/order/BaseEntity.java
@@ -7,17 +7,20 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter @Setter
-public class BaseEntity {
+public abstract class BaseEntity {
 
     @CreatedBy
+    @Column(updatable = false)
     private String createdBy;
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @LastModifiedBy
