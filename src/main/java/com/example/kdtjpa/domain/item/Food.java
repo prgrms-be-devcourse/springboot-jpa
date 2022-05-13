eec0712ee295c263f9869ce252e55f2378ee461b
hayoung <60775067+gkdud583@users.noreply.github.com>
2022-05-13 15:40:56 +0900

    Update Food.java

--- src/main/java/com/example/kdtjpa/domain/item/Food.java
+++ src/main/java/com/example/kdtjpa/domain/item/Food.java
@@ -5,7 +5,6 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import static lombok.AccessLevel.PROTECTED;
 
-
 @DiscriminatorValue("FOOD")
 @NoArgsConstructor(access = PROTECTED)
 @Entity
