d988eb91deadfca30fdc1d102659e918c7f47eb0
dojinyou <djyou128@gmail.com>
2022-05-15 02:01:01 +0900

    :fire: chore: 연습용 파일 file 삭제

--- src/main/java/com/dojinyou/devcourse/springbootjpa/practice/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dojinyou.devcourse.springbootjpa.practice.repository;
-
-import com.dojinyou.devcourse.springbootjpa.practice.repository.domain.CustomerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
-
-}
