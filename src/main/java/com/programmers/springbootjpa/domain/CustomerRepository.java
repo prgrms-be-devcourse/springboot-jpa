a21d5306541c74d7bded849b3cefcc10f19d2bf0
HyeonseoJUNG <windymay1920@gmail.com>
2022-05-14 20:37:07 +0900

    Refactor: 사용하지 않는 코드 삭제

--- src/main/java/com/programmers/springbootjpa/domain/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.programmers.springbootjpa.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
