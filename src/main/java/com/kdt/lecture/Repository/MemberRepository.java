c63fbc94984fba3145793d9b00da1b3058a0712b
Hyunggeun447 <bopi447@naver.com>
2022-05-14 12:50:34 +0900

    jpa repo 생성

--- src/main/java/com/kdt/lecture/Repository/MemberRepository.java
+++ src/main/java/com/kdt/lecture/Repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package com.kdt.lecture.Repository;
 
+import com.kdt.lecture.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository  {
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
 }
