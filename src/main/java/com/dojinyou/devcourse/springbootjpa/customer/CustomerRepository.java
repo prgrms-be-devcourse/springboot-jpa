d3ee88cf58d8bccfc4fa0b2db92866e874bd4e59
프로그래머스 교육팀 <learn@programmers.co.kr>
2022-05-15 21:48:28 +0900

    Revert "[2기-C]유도진-Mission1:단일 엔티티를 이용한 CRUD를 구현"

--- src/main/java/com/dojinyou/devcourse/springbootjpa/customer/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.dojinyou.devcourse.springbootjpa.customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
-}
--- src/test/java/com/dojinyou/devcourse/springbootjpa/SpringbootJpaApplicationTests.java
+++
@@ -1,7 +0,0 @@
-package com.dojinyou.devcourse.springbootjpa;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringbootJpaApplicationTests {
-}
