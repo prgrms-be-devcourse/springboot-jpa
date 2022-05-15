65dd0546ab4fe68bcf811d1bfa61407284a14b79
ecvheo1 <ecvheo@naver.com>
2022-05-15 12:11:44 +0900

    :tada: 프로젝트 시작

---
+++ src/test/java/com/kdt/JpaWeekly/JpaWeeklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.JpaWeekly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWeeklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
