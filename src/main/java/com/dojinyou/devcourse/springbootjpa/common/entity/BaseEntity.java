a5a46cad78d3a505cb2691c1b0d12fdc88c6817d
dojinyou <djyou128@gmail.com>
2022-05-16 17:31:04 +0900

    :sparkles: feat: BaseEntity 추상 객체로 전환

--- src/main/java/com/dojinyou/devcourse/springbootjpa/common/entity/BaseEntity.java
+++ src/main/java/com/dojinyou/devcourse/springbootjpa/common/entity/BaseEntity.java
@@ -9,7 +9,7 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
 
     @CreationTimestamp
     @Column(columnDefinition = "TIMESTAMP")
