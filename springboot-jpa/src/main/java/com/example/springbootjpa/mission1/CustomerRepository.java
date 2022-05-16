74ccbb0c3d4d2b307b89d7addd882f90aa349ad8
Ella Ma <tiger980424@naver.com>
2022-05-16 17:53:14 +0900

    Revert "[2기-P] 마지영 Mission 2: 영속성 컨텍스트 & Mission 3: 연관관계 매핑"

--- springboot-jpa/src/main/java/com/example/springbootjpa/mission1/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.springbootjpa.mission1;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
