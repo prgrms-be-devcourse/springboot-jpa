58dd2ab05e6fab346293c111d3d560dc22d4308c
yerimmac <yerim.lee.earth@gmail.com>
2022-05-23 22:11:04 +0900

    refactor : enum 코드 타입을 변경한다

--- demo/src/main/java/com/example/demo/converter/OrderStatusConverter.java
+++ demo/src/main/java/com/example/demo/converter/OrderStatusConverter.java
@@ -1,17 +1,17 @@
 package com.example.demo.converter;
 
-import com.example.demo.domain.OrderStatus;
+import com.example.demo.domain.order.OrderStatus;
 
 import javax.persistence.AttributeConverter;
 
-public class OrderStatusConverter implements AttributeConverter<OrderStatus, String> {
+public class OrderStatusConverter implements AttributeConverter<OrderStatus, Integer> {
     @Override
-    public String convertToDatabaseColumn(OrderStatus attribute) {
+    public Integer convertToDatabaseColumn(OrderStatus attribute) {
         return attribute.getCode();
     }
 
     @Override
-    public OrderStatus convertToEntityAttribute(String dbData) {
+    public OrderStatus convertToEntityAttribute(Integer dbData) {
         return OrderStatus.ofCode(dbData);
     }
 }
