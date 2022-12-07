0886c1eeb4857c4c53ee8625a50859443db42104
kivv00ng <rldnd2637@naver.com>
2022-12-07 18:46:49 +0900

    미션1 crud

---
+++ LectureJpa/src/test/java/com/kdt/lecture/LectureJpa/LectureJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.lecture.LectureJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
