31164457e0f784642c7b8bff392efe1afe0920ed
hyunseo <goatsfish@gmail.com>
2022-12-08 00:39:58 +0900

    feat : 고객 저장, 검색, 수정, 삭제 기능 추가

---
+++ src/main/java/com/devcourse/mission/domain/customer/entity/Customer.java
@@ -0,0 +1,24 @@
+package com.devcourse.mission.domain.customer.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Table(name = "customers")
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column( nullable = false, length = 30)
+    private String address;
+
+    @Column(nullable = false, length = 30)
+    private int age;
+}
