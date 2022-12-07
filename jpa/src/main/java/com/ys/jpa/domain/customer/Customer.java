84307634b31ff13ae0c97970d2edf3758605a0a3
ysk <kim206gh@naver.com>
2022-12-07 15:41:30 +0900

    feat : Customer 엔티티 생성

---
+++ jpa/src/main/java/com/ys/jpa/domain/customer/Customer.java
@@ -0,0 +1,40 @@
+package com.ys.jpa.domain.customer;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false) @Comment("이름")
+    private String firstName;
+
+    @Column(nullable = false) @Comment("성")
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        Assert.isTrue(StringUtils.hasText(firstName), "이름은 빈 값이면 안됩니다.");
+        Assert.isTrue(StringUtils.hasText(lastName), "성은 빈 값이면 안됩니다.");
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
---
+++ jpa/src/test/java/com/ys/jpa/domain/customer/CustomerRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.ys.jpa.domain.customer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+}
