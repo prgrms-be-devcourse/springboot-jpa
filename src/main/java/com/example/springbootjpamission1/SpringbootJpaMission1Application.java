4808382d3f1c7167cd55cd23f0e01809c93ac339
cloudwi <cloudwi@naver.com>
2022-12-08 00:57:52 +0900

    초기 세팅 입니다. springboot maven

---
+++ src/main/java/com/example/springbootjpamission1/SpringbootJpaMission1Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootjpamission1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaMission1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaMission1Application.class, args);
+    }
+
+}
