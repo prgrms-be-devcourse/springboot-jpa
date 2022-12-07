13154c9d9131558316d16aa33e4999dfb0914eae
김기웅 <93498749+kivv00ng@users.noreply.github.com>
2022-12-08 00:51:20 +0900

    Revert "[3기-C 김기웅] Mission 1: JPA 소개"

--- LectureJpa/src/main/java/com/kdt/lecture/LectureJpa/domain/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.kdt.lecture.LectureJpa.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
