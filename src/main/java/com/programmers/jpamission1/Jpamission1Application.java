50111a6bbf30cc828e0a584e8a9a4529389583bb
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-12-09 15:31:26 +0900

    initial commit

---
+++ src/main/java/com/programmers/jpamission1/Jpamission1Application.java
@@ -0,0 +1,13 @@
+package com.programmers.jpamission1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpamission1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpamission1Application.class, args);
+	}
+
+}
