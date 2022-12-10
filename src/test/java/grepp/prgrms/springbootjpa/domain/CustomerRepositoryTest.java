09d0d873f972a01f50eb73b440f5871a682fd09d
Hwan Kim <hwankim1941@gmail.com>
2022-12-10 17:17:18 +0900

    feat: Customer CRUD

---
+++ src/test/java/grepp/prgrms/springbootjpa/domain/CustomerRepositoryTest.java
@@ -0,0 +1,98 @@
+package grepp.prgrms.springbootjpa.domain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @AfterEach
+    void tearDown(){
+        customerRepository.deleteAll();
+    }
+
+    @Test
+    @Transactional
+    void insert(){
+        //given
+        Long id = 1L;
+        String firstName = "first";
+        String lastName = "last";
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customerRepository.save(customer);
+
+        //when
+        Customer found = customerRepository.findById(id).get();
+
+        //then
+        assertThat(found.getId()).isEqualTo(id);
+        assertThat(found.getFirstName()).isEqualTo(firstName);
+        assertThat(found.getLastName()).isEqualTo(lastName);
+    }
+
+    @Test
+    @Transactional
+    void update(){
+        //given
+        Long id = 1L;
+        String firstName = "first";
+        String lastName = "last";
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customerRepository.save(customer);
+
+        //when
+        Customer found = customerRepository.findById(1L).get();
+        String newFirstName = "newFirst";
+        String newLastName = "newLast";
+        found.setFirstName(newFirstName);
+        found.setLastName(newLastName);
+        Customer foundAfterUpdate = customerRepository.findById(1L).get();
+
+        //then
+        assertThat(foundAfterUpdate.getId()).isEqualTo(id);
+        assertThat(foundAfterUpdate.getFirstName()).isEqualTo(newFirstName);
+        assertThat(foundAfterUpdate.getLastName()).isEqualTo(newLastName);
+
+    }
+
+    @Test
+    @Transactional
+    void delete(){
+        //given
+        Long id = 1L;
+        String firstName = "first";
+        String lastName = "last";
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        Customer saved = customerRepository.save(customer);
+
+        //when
+        customerRepository.delete(saved);
+
+        //then
+        Optional<Customer> found = customerRepository.findById(id);
+
+        assertThat(found).isEmpty();
+
+    }
+}
