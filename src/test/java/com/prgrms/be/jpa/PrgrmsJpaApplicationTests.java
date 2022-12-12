6c0d32b6eff334d6d932a523c36658a3a931e5d3
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-12-12 10:40:23 +0900

    feat: Application에 관한 클래스, 테스트를 추가하다.

---
+++ src/test/java/com/prgrms/be/jpa/PrgrmsJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.be.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrgrmsJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
