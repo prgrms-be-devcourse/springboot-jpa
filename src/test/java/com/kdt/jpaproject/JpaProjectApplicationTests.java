a11eae79188d499eeb0aa1f8fd93630aad0de465
hikarigin99 <hikarigin99@gmail.com>
2022-12-12 18:46:55 +0900

    add: jpa project init

---
+++ src/test/java/com/kdt/jpaproject/JpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.jpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
