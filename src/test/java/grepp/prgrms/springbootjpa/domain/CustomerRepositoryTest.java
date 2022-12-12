2655553100d8410c601f049b5d537f4718add609
Hwan Kim <hwankim1941@gmail.com>
2022-12-12 23:01:00 +0900

    refactor: 리뷰 반영

--- src/test/java/grepp/prgrms/springbootjpa/domain/CustomerRepositoryTest.java
+++ src/test/java/grepp/prgrms/springbootjpa/domain/CustomerRepositoryTest.java
@@ -1,10 +1,9 @@
 package grepp.prgrms.springbootjpa.domain;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -12,16 +11,11 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
-@SpringBootTest
+@DataJpaTest
 class CustomerRepositoryTest {
 
     @Autowired
     CustomerRepository customerRepository;
-
-    @AfterEach
-    void tearDown(){
-        customerRepository.deleteAll();
-    }
 
     @Test
     @Transactional
@@ -93,6 +87,7 @@ class CustomerRepositoryTest {
         Optional<Customer> found = customerRepository.findById(id);
 
         assertThat(found).isEmpty();
+
 
     }
 }
