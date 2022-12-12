a4385a2d6e23cc34042f02f8b32311b21fd924a9
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-12-12 12:14:21 +0900

    fix: optimizing import를 수행하다.

--- src/main/java/com/prgrms/be/jpa/domain/Customer.java
+++ src/main/java/com/prgrms/be/jpa/domain/Customer.java
@@ -3,7 +3,6 @@ package com.prgrms.be.jpa.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
--- src/test/java/com/prgrms/be/jpa/domain/CustomerTest.java
+++ src/test/java/com/prgrms/be/jpa/domain/CustomerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
