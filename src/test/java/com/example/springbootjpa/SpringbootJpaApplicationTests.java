7cb180abd6d8360e44b64018b726e282ec1cf2fc
boompatron <grapeflavoredapple@gmail.com>
2022-12-13 15:17:26 +0900

    [test] testcontainer 설정

--- src/test/java/com/example/springbootjpa/SpringbootJpaApplicationTests.java
+++ src/test/java/com/example/springbootjpa/SpringbootJpaApplicationTests.java
@@ -3,11 +3,13 @@ package com.example.springbootjpa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class SpringbootJpaApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("hello world");
 	}
 
 }
