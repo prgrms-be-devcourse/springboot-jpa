91749c0b56c87d1cf4d6f7cc54eadda7cd8f8416
boompatron <grapeflavoredapple@gmail.com>
2022-12-13 15:56:54 +0900

    [feat] customer service 구현

---
+++ src/main/java/com/example/springbootjpa/customer/service/CustomerService.java
@@ -0,0 +1,37 @@
+package com.example.springbootjpa.customer.service;
+
+import com.example.springbootjpa.customer.model.Customer;
+import com.example.springbootjpa.customer.repository.CustomerRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerService {
+    private final CustomerRepository repository;
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    Customer save(Customer customer){
+        return repository.save(customer);
+    }
+
+    Optional<Customer> findById(Long id){
+        return repository.findById(id);
+    }
+
+    List<Customer> findAll(){
+        return repository.findAll();
+    }
+
+    Customer update(Customer customer){
+        return customer;
+    }
+
+    void deleteById(Long id){
+        repository.deleteById(id);
+    }
+    void deleteAll(){
+        repository.deleteAll();
+    }
+}
