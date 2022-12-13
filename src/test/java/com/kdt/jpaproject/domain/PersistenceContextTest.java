da02c35ea86371f12d4fe85c06974c2843d01a62
hikarigin99 <hikarigin99@gmail.com>
2022-12-13 16:47:27 +0900

    add: assertj test

--- src/test/java/com/kdt/jpaproject/domain/PersistenceContextTest.java
+++ src/test/java/com/kdt/jpaproject/domain/PersistenceContextTest.java
@@ -10,6 +10,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Slf4j
 @SpringBootTest
 class PersistenceContextTest {
@@ -59,6 +61,11 @@ class PersistenceContextTest {
 
         Customer selectedCustomer = entityManager.find(Customer.class, 1L);     // select
         log.info("{} {}", selectedCustomer.getFirstName(), selectedCustomer.getFirstName());
+
+        assertThat(selectedCustomer)
+                .hasFieldOrPropertyWithValue("id", customer.getId())
+                .hasFieldOrPropertyWithValue("firstName", customer.getFirstName())
+                .hasFieldOrPropertyWithValue("lastName", customer.getLastName());
     }
 
     @Test
@@ -79,6 +86,7 @@ class PersistenceContextTest {
 
         Customer selectedCustomer = entityManager.find(Customer.class, 1L);
         log.info("{} {}", selectedCustomer.getFirstName(), selectedCustomer.getFirstName());
+        assertThat(selectedCustomer).usingRecursiveComparison().isEqualTo(customer);
     }
 
     @Test
@@ -102,6 +110,27 @@ class PersistenceContextTest {
     }
 
     @Test
+    void 조회_DB조회_merge() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("eunbi");
+        customer.setLastName("choi");
+        entityManager.persist(customer);
+        transaction.commit();
+
+        entityManager.detach(customer);
+        Customer merge = entityManager.merge(customer);
+
+        Customer mergedCustomer = entityManager.find(Customer.class, 1L);
+        log.info("{} {}", mergedCustomer.getFirstName(), mergedCustomer.getFirstName());
+        assertThat(mergedCustomer).usingRecursiveComparison().isEqualTo(merge);
+    }
+
+    @Test
     void 조회_1차캐시_이용() {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
@@ -116,26 +145,7 @@ class PersistenceContextTest {
 
         Customer selectedCustomer = entityManager.find(Customer.class, 1L);     // 1차 캐시
         log.info("{} {}", selectedCustomer.getFirstName(), selectedCustomer.getFirstName());
-    }
-
-    @Test
-    void 조회_1차캐시_이용_merge() {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        EntityTransaction transaction = entityManager.getTransaction();
-
-        transaction.begin();
-        Customer customer = new Customer();
-        customer.setId(1L);
-        customer.setFirstName("eunbi");
-        customer.setLastName("choi");
-        entityManager.persist(customer);
-        transaction.commit();
-
-        entityManager.detach(customer);
-        entityManager.merge(customer);
-
-        Customer mergedCustomer = entityManager.find(Customer.class, 1L);     // 1차 캐시 이용
-        log.info("{} {}", mergedCustomer.getFirstName(), mergedCustomer.getFirstName());
+        assertThat(selectedCustomer).usingRecursiveComparison().isEqualTo(customer);
     }
 
     @Test
@@ -156,8 +166,9 @@ class PersistenceContextTest {
         customer.setLastName("guppy");
         transaction.commit();               // update
 
-        Customer selectedCustomer = entityManager.find(Customer.class, 1L);
-        log.info("{} {}", selectedCustomer.getFirstName(), selectedCustomer.getLastName());
+        Customer updatedCustomer = entityManager.find(Customer.class, 1L);
+        log.info("{} {}", updatedCustomer.getFirstName(), updatedCustomer.getLastName());
+        assertThat(updatedCustomer).usingRecursiveComparison().isEqualTo(customer);
     }
 
     @Test
@@ -176,5 +187,7 @@ class PersistenceContextTest {
         transaction.begin();
         entityManager.remove(customer);     // 영속 -> 비영속 (DB삭제)
         transaction.commit();               // delete
+
+        assertThat(entityManager.find(Customer.class, 1L)).isNull();
     }
 }
