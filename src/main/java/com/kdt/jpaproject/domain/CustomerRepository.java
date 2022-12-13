1f58ea2c018218020785b8f36ee592cb93dfb98f
hikarigin99 <59335077+hikarigin99@users.noreply.github.com>
2022-12-13 16:56:04 +0900

    Revert "[3기-C 최은비] Mission 2 : 영속성 컨텍스트"

--- src/main/java/com/kdt/jpaproject/domain/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kdt.jpaproject.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
-
