b868893c652ba327e672d05c00a481c2b0149cef
goseungwon <knmy0925@gmail.com>
2022-12-14 23:09:41 +0900

    [Feat] member entity 작성

---
+++ src/main/java/com/prgrms/jpamission/domain/Member.java
@@ -0,0 +1,43 @@
+package com.prgrms.jpamission.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "members")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @Column(name = "id")
+    private String uuid;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "nick_name", unique = true, nullable = false)
+    private String nickName;
+
+    @Column(length = 3, nullable = false)
+    private int age;
+
+    @Column(unique = true, nullable = false)
+    private String address;
+    private String description;
+
+    @OneToMany(mappedBy = "members")
+    private List<Order> orders = new ArrayList<>();
+
+    public void addOrder(Order order) {
+        order.setMember(this);
+    }
+
+}
