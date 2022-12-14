1e912d44d09ee7c6c5893cc19eb6c24776932baf
권성준 <tjdwns1865@naver.com>
2022-12-14 15:19:32 +0900

    refactor: BaseEntity 에 Getter 추가

--- src/main/java/com/programmers/jpapractice/domain/order/BaseEntity.java
+++ src/main/java/com/programmers/jpapractice/domain/order/BaseEntity.java
@@ -5,7 +5,10 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+import lombok.Getter;
+
 @MappedSuperclass
+@Getter
 public class BaseEntity {
 
 	private String createdBy;
