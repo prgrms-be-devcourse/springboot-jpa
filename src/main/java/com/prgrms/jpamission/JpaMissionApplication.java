ca6630ce759e30a2563045a09247587af9c37969
goseungwon <knmy0925@gmail.com>
2022-12-14 19:11:33 +0900

    [Chore] 프로젝트 생성

---
+++ src/main/java/com/prgrms/jpamission/JpaMissionApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.jpamission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMissionApplication.class, args);
+    }
+
+}
