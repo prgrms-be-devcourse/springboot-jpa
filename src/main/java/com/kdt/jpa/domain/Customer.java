91febbe1a87e156011f6314cd84b7edfd722ff84
ttaehee <ttaehee1105@gmail.com>
2022-12-20 22:41:27 +0900

    Refactor : id에 @GeneratedValue 추가

--- src/main/java/com/kdt/jpa/domain/Customer.java
+++ src/main/java/com/kdt/jpa/domain/Customer.java
@@ -1,6 +1,8 @@
 package com.kdt.jpa.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -8,6 +10,7 @@ import javax.persistence.Table;
 @Table(name = "customers")
 public class Customer {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String firstName;
     private String lastName;
