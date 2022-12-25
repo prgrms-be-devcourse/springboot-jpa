f0b2abcaf3ca65a688f24303b0087427b5b62014
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-12-25 20:02:39 +0900

    feat: JpaAuditing을 이용하여 BaseEntity 필드를 자동으로 변경되게 설정하다.

---
+++ src/main/java/com/prgrms/be/jpa/domain/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.prgrms.be.jpa.domain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
