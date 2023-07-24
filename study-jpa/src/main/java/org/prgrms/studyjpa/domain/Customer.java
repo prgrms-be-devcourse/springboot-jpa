503f42909f0f06380a3bb6dad4881eda53eba22b
ddongpuri <fnffnfkffk1112@gmail.com>
2023-07-24 23:49:51 +0900

    feat: 테이블명 변경 및 DB 설정 추가

--- study-jpa/src/main/java/org/prgrms/studyjpa/domain/Customer.java
+++ study-jpa/src/main/java/org/prgrms/studyjpa/domain/Customer.java
@@ -2,10 +2,12 @@ package org.prgrms.studyjpa.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
 @Getter
 @Entity
+@Table(name = "customers")
 public class Customer {
     @Id
     private long id;
