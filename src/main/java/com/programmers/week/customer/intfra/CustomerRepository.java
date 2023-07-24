798037f87c108ef3ffea97c9ceca000e3f09a0d3
박은지 <qkrdmswl1018@naver.com>
2023-07-24 22:49:03 +0900

    feat: CustomerRepository 작성

---
+++ src/main/java/com/programmers/week/customer/intfra/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.programmers.week.customer.intfra;
+
+import com.programmers.week.customer.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
