d8cf13556b4f143c8499c03edcd5541988f97f32
juno-junho <ssmm0205@naver.com>
2023-07-24 23:48:57 +0900

    feat(domain): customer entity 생성

--- kdt-jpa/src/main/java/com/kdt/kdtjpa/domain/Customer.java
+++ kdt-jpa/src/main/java/com/kdt/kdtjpa/domain/Customer.java
@@ -3,12 +3,14 @@ package com.kdt.kdtjpa.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Table(name = "customers")
 @Getter @Setter
+@EqualsAndHashCode
 public class Customer {
     @Id
     private long id;
