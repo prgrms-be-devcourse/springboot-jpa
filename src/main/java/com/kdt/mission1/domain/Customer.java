b576b6e688d620072fbad3905f83f3d29bc8fea5
Choi Seongwon <vlflalektzja@naver.com>
2023-07-24 17:23:12 +0900

    feat: Customer getter/setter 추가

--- src/main/java/com/kdt/mission1/domain/Customer.java
+++ src/main/java/com/kdt/mission1/domain/Customer.java
@@ -1,9 +1,14 @@
 package com.kdt.mission1.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Customer {
     @Id
     private long id;
