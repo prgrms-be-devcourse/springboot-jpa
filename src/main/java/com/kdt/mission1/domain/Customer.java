2cd9774b5efebc6900fe945166bb8d32b8878018
Choi Seongwon <vlflalektzja@naver.com>
2023-07-24 22:42:45 +0900

    feat: Customer getter 추가

--- src/main/java/com/kdt/mission1/domain/Customer.java
+++ src/main/java/com/kdt/mission1/domain/Customer.java
@@ -1,12 +1,27 @@
 package com.kdt.mission1.domain;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Getter
 public class Customer {
     @Id
     private long id;
     private String firstName;
     private String lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Customer() {
+        this.id = 0L;
+        this.firstName = "firstname";
+        this.lastName = "lastname";
+    }
 }
