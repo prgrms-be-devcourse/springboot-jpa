e69b3afa6f4d8aeefa491e070dbc3f56df464af8
박은지 <qkrdmswl1018@naver.com>
2023-07-25 00:55:36 +0900

    feat: CustomerService 작성

--- src/main/java/com/programmers/week/customer/application/CustomerService.java
+++ src/main/java/com/programmers/week/customer/application/CustomerService.java
@@ -3,9 +3,13 @@ package com.programmers.week.customer.application;
 import com.programmers.week.customer.ui.CustomerCreateRequest;
 import com.programmers.week.customer.domain.Customer;
 import com.programmers.week.customer.infra.CustomerRepository;
+import com.programmers.week.customer.ui.CustomerResponse;
+import com.programmers.week.customer.ui.CustomerUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -14,6 +18,7 @@ public class CustomerService {
 
   private final CustomerRepository customerRepository;
 
+  @Transactional
   public Long create(CustomerCreateRequest createRequest) {
     Customer customer = new Customer(
             createRequest.firstName(),
@@ -21,6 +26,34 @@ public class CustomerService {
     );
     Customer saved = customerRepository.save(customer);
     return saved.getId();
+  }
+
+  public CustomerResponse findById(Long id) {
+    return customerRepository.findById(id)
+            .map(CustomerResponse::from)
+            .orElseThrow(() -> new IllegalArgumentException("고객이 존재하지 않습니다."));
+  }
+
+  public List<CustomerResponse> findAll() {
+    List<Customer> customers = customerRepository.findAll();
+    return customers.stream()
+            .map(CustomerResponse::from)
+            .toList();
+  }
+
+  @Transactional
+  public Long update(CustomerUpdateRequest customerUpdateRequest) {
+    System.out.println("test=======> " + customerUpdateRequest.toString());
+    Customer customer = customerRepository.findById(customerUpdateRequest.id())
+            .orElseThrow(() -> new IllegalArgumentException("고객이 존재하지 않습니다."));
+    customer.changeFirstName(customerUpdateRequest.firstName());
+    customer.changeLastName(customerUpdateRequest.lastName());
+    return customer.getId();
+  }
+
+  @Transactional
+  public void deleteById(Long id) {
+    customerRepository.deleteById(id);
   }
 
 }
