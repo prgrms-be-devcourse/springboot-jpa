894447bcd655f28c25d365ba635fbe1f507f1676
ymkim97 <rladudaud97@naver.com>
2023-07-25 01:51:31 +0900

    init

---
+++ src/main/java/com/kdt/weeklyjpa/WeeklyJpaApplication.java
@@ -0,0 +1,13 @@
+package com.kdt.weeklyjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeeklyJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kdt/weeklyjpa/WeeklyJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.weeklyjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
