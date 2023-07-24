a184c3a9d35c6429b4fcedcaecc578520911c289
HyuckJuneHong <jjj8337721@naver.com>
2023-07-24 22:09:50 +0900

    init : Project 초기화

---
+++ src/main/java/kr/co/springbootjpaweeklymission/SpringbootJpaWeeklyMissionApplication.java
@@ -0,0 +1,13 @@
+package kr.co.springbootjpaweeklymission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaWeeklyMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaWeeklyMissionApplication.class, args);
+    }
+
+}
