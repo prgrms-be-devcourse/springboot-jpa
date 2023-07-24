c65fcb2d426662f86bc9b5da959526704114800b
박은지 <qkrdmswl1018@naver.com>
2023-07-25 00:20:09 +0900

    feat: Customer 엔티티 수정

--- src/main/java/com/programmers/week/customer/domain/Customer.java
+++ src/main/java/com/programmers/week/customer/domain/Customer.java
@@ -1,17 +1,16 @@
 package com.programmers.week.customer.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
+
+import java.util.Objects;
 
 @Entity
 @Getter
 public class Customer {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @Column(length = 5, nullable = false)
@@ -24,8 +23,32 @@ public class Customer {
   }
 
   public Customer(String firstName, String lastName) {
+    validateFirstName(firstName);
+    validateLastName(lastName);
     this.firstName = firstName;
     this.lastName = lastName;
+  }
+
+  public void changeFirstName(String firstName) {
+    validateFirstName(firstName);
+    this.firstName = firstName;
+  }
+
+  private static void validateFirstName(String firstName) {
+    if (Objects.isNull(firstName) || firstName.isBlank()) {
+      throw new IllegalStateException("이름이 비어있습니다.");
+    }
+  }
+
+  public void changeLastName(String lastName) {
+    validateLastName(lastName);
+    this.lastName = lastName;
+  }
+
+  private static void validateLastName(String lastName) {
+    if (Objects.isNull(lastName) || lastName.isBlank()) {
+      throw new IllegalStateException("성이 비어있습니다.");
+    }
   }
 
 }
