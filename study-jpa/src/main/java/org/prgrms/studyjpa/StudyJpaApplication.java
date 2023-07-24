d9c2f2f8c7990aaedbcb1935549255e76f259998
ddongpuri <fnffnfkffk1112@gmail.com>
2023-07-24 20:42:26 +0900

    init: 프로젝트 생성 및 초기화

---
+++ study-jpa/src/main/java/org/prgrms/studyjpa/StudyJpaApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.studyjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJpaApplication.class, args);
+	}
+
+}
