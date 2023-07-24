a3acedcdae2a3813da6e32b2e4bc1a5347e09cdd
박은지 <qkrdmswl1018@naver.com>
2023-07-24 21:35:20 +0900

    initial commit

---
+++ src/main/java/com/programmers/week/WeekApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(WeekApplication.class, args);
+  }
+
+}
+
