cd8bfe8d95caf246f9437c3e2d8c0629a0c38862
ymkim97 <rladudaud97@naver.com>
2023-07-25 15:56:18 +0900

    Customer read test

--- src/test/java/com/kdt/weeklyjpa/domain/CustomerRepositoryTest.java
+++ src/test/java/com/kdt/weeklyjpa/domain/CustomerRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,6 +42,37 @@ class CustomerRepositoryTest {
                 .get()
                 .usingRecursiveComparison()
                 .isEqualTo(customer);
+    }
+
+    @Test
+    @DisplayName("Customers_조회_테스트")
+    void getCustomerTest() {
+        // given
+        Customer customer1 = new Customer("Youngmyung", "Kim");
+        Customer customer2 = new Customer("Test1", "Hong");
+        Customer customer3 = new Customer("Test2", "Kwon");
+
+
+        // when
+        customerRepository.save(customer1);
+        customerRepository.save(customer2);
+        customerRepository.save(customer3);
+        List<Customer> customers = customerRepository.findAll();
+
+        // then
+        assertThat(customers).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("Customer_update_테스트")
+    void updateCustomerTest() {
+        // given
+        Customer customer1 = new Customer("Youngmyung", "Kim");
+
+
+        // when
+
+        // then
     }
 
 }
