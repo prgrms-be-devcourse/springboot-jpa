515dca6bb9c92dd0669bbe472bc939723fb80691
bona <ra_ari@naver.com>
2023-07-25 11:15:17 -0700

    [feat] domain - Customer, CustomerRepository [test] CustomerRepositoryTest

--- src/main/java/com/example/weeklyjpa/domain/Customer.java
+++ src/main/java/com/example/weeklyjpa/domain/Customer.java
@@ -8,7 +8,7 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Customer {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
 
     @Column
