92f18de09e8f8b43cde5f452ed718f8c30c30fce
Sunny <rosa7536@naver.com>
2023-07-25 22:21:05 +0900

    [FEAT] CRUD test

--- src/test/java/com/kdt/lecture/domain/CustomerRepositoryTest.java
+++ src/test/java/com/kdt/lecture/domain/CustomerRepositoryTest.java
@@ -1,32 +1,84 @@
 package com.kdt.lecture.domain;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
-@SpringBootTest
+@DataJpaTest
 class CustomerRepositoryTest {
+
+    static Customer customer;
 
     @Autowired
     private CustomerRepository repository;
 
-    @Test
-    void test() {
-        // Given
-        Customer customer = new Customer();
-        customer.setFirstName("honggu");
-        customer.setLastName("Kang");
-//        customer.setAge(33);
+    @BeforeEach
+    void setUp() {
+        customer = Customer.builder()
+                .firstName("SeonHeui")
+                .lastName("Jeon")
+                .build();
+    }
 
-        // When
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll();
+    }
+
+    @DisplayName("Customer Create Test")
+    @Test
+    void testCreateCustomer() {
         repository.save(customer);
 
-        // Then
-        Customer entity = repository.findById(1L).get();
-        log.info("{} {}", entity.getLastName(), entity.getFirstName());
+        assertNotNull(customer.getId());
+        assertEquals(customer.getFirstName(), "SeonHeui");
+        assertEquals(customer.getLastName(), "Jeon");
+    }
+
+    @DisplayName("Customer Read Test")
+    @Test
+    void testReadCustomer() {
+        repository.save(customer);
+        Customer readCustomer = repository.findById(customer.getId()).get();
+
+        assertEquals(customer, readCustomer);
+    }
+
+    @DisplayName("Customer Update Test")
+    @Test
+    void testUpdateCustomer() {
+        repository.save(customer);
+        Customer readCustomer = repository.findById(customer.getId()).get();
+
+        readCustomer.updateCustomerName("mingky", "Jeon");
+
+        assertEquals(customer.getFirstName(), readCustomer.getFirstName());
+        assertEquals(customer.getLastName(), readCustomer.getLastName());
+    }
+
+    @DisplayName("Customer Delete All Test")
+    @Test
+    void testDeleteAllCustomer() {
+        repository.save(customer);
+
+        repository.deleteAll();
+
+        assertEquals(repository.count(), 0);
+    }
+
+    @DisplayName("Customer Delete Test")
+    @Test
+    void testDeleteCustomer() {
+        repository.save(customer);
+
+        repository.deleteById(customer.getId());
+
+        Customer readCustomer = repository.findById(customer.getId()).orElse(null);
+        assertNull(readCustomer);
     }
 }
