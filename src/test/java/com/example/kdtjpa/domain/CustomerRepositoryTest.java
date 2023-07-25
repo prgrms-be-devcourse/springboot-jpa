b14e5b8e68354ef161554ebf1860f2c732ea9156
sujjangOvO <pnk7038@gmail.com>
2023-07-25 13:59:28 +0900

    Test 환경 재설정

--- src/test/java/com/example/kdtjpa/domain/CustomerRepositoryTest.java
+++ src/test/java/com/example/kdtjpa/domain/CustomerRepositoryTest.java
@@ -2,7 +2,6 @@ package com.example.kdtjpa.domain;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,10 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @SpringBootTest
 @Transactional
-public class CustomerRepositoryTest {
+class CustomerRepositoryTest {
 
 	@Autowired
-	private CustomerRepository repository;
+	CustomerRepository repository;
 
 	@Test
 	void test() {
