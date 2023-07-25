1186996c5b64765efbb9fdfebce6b2ea13173723
bona <ra_ari@naver.com>
2023-07-25 10:50:04 -0700

    [feat] domain - Customer, CustomerRepository [test] CustomerRepositoryTest

---
+++ src/main/java/com/example/weeklyjpa/domain/Customer.java
@@ -0,0 +1,32 @@
+package com.example.weeklyjpa.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Customer {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void updateFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void updateLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/test/java/com/example/weeklyjpa/domain/CustomerRepositoryTest.java
@@ -0,0 +1,92 @@
+package com.example.weeklyjpa.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    private Customer customer;
+
+    @BeforeEach
+    void setUp() {
+        customer = new Customer("yejin","shin");
+    }
+
+    @AfterEach
+    void clear() {
+        customerRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("고객을 저장한다.")
+    void saveTest(){
+        // when
+        Customer savedCustomer = customerRepository.save(customer);
+        // then
+        assertThat(savedCustomer.getFirstName()).isEqualTo(customer.getFirstName());
+    }
+
+    @Test
+    @DisplayName("전체 고객을 조회한다.")
+    void findAllTest() {
+        // when
+        customerRepository.save(customer);
+        List<Customer> result = customerRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("id로 고객을 조회한다.")
+    void findByIdTest() {
+        // given
+        Customer savedCustomer = customerRepository.save(customer);
+        long customerId = savedCustomer.getId();
+        // when
+        Customer findCustomerById = customerRepository.findById(customerId).get();
+
+        // then
+        assertThat(findCustomerById).isEqualTo(savedCustomer);
+    }
+
+    @Test
+    @DisplayName("고객의 정보를 업데이트한다.")
+    void updateTest() {
+        // given
+        Customer savedCustomer = customerRepository.save(customer);
+
+        // when
+        String updateName = "zara";
+        savedCustomer.updateFirstName(updateName);
+
+        // then
+        assertThat(savedCustomer.getFirstName()).isEqualTo(updateName);
+    }
+
+    @Test
+    @DisplayName("고객 id로 고객을 삭제한다.")
+    void deleteByIdTest() {
+        // given
+        Customer savedCustomer = customerRepository.save(customer);
+        long customerId = savedCustomer.getId();
+
+        // when
+        customerRepository.deleteById(customerId);
+
+        // then
+        assertThat(customerRepository.findAll().size()).isEqualTo(0);
+    }
+}
