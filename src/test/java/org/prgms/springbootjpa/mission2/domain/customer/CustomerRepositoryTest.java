d0ab3a26116406f7f4867195dcb948af6b37e8e6
hyeon-z <phj8221@naver.com>
2023-07-26 06:09:32 +0900

    test: 영속성 컨텍스트 테스트
    CustomerRepository의 CRUD 메서드 활용

---
+++ src/test/java/org/prgms/springbootjpa/mission2/domain/customer/CustomerRepositoryTest.java
@@ -0,0 +1,143 @@
+package org.prgms.springbootjpa.mission2.domain.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.prgms.springbootjpa.mission1.customer.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest
+class CustomerRepositoryTest {
+    @Autowired
+    CustomerRepository repository;
+
+    @Autowired
+    EntityManagerFactory emf;
+
+    @BeforeEach
+    void setUp() {
+        repository.deleteAll();
+    }
+
+    Customer customer = new Customer(1L, "hyeonji", "park");
+
+    @Test
+    void 저장() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        entityManager.persist(customer);
+
+        transaction.commit();
+    }
+
+    @Test
+    void 조회_1차캐시_이용() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        entityManager.persist(customer);
+
+        transaction.commit();
+
+        Customer entity = entityManager.find(Customer.class, customer.getId());
+        assertThat(entity, samePropertyValuesAs(customer));
+    }
+
+
+    @Test
+    void 조회_DB_이용() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        entityManager.persist(customer);
+
+        transaction.commit();
+
+        entityManager.detach(customer);
+
+        Customer entity = entityManager.find(Customer.class, customer.getId());
+        assertThat(entity, samePropertyValuesAs(customer));
+    }
+
+    @Test
+    void 수정_dirtyCheck_적용() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        entityManager.persist(customer);
+
+        transaction.commit();
+
+        transaction.begin();
+
+        customer.changeFirstName("hyeonz");
+
+        transaction.commit();
+
+        Customer entity = entityManager.find(Customer.class, customer.getId());
+        assertThat(entity.getFirstName(), is("hyeonz"));
+    }
+
+    @Test
+    void 수정_dirtyCheck_적용_안됨() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        entityManager.persist(customer);
+
+        transaction.commit();
+
+        entityManager.detach(customer);
+
+        transaction.begin();
+
+        customer.changeFirstName("hyeonz");
+
+        transaction.commit();
+
+        Customer afterEntity = entityManager.find(Customer.class, customer.getId());
+        assertThat(afterEntity.getFirstName(), not("hyeonz"));
+    }
+
+    @Test
+    void 삭제() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        entityManager.persist(customer);
+
+        transaction.commit();
+
+        transaction.begin();
+
+        Customer beforeCommit = entityManager.find(Customer.class, customer.getId());
+        assertThat(beforeCommit, samePropertyValuesAs(customer));
+
+        entityManager.remove(customer);
+
+        transaction.commit();
+
+        Customer afterCommit = entityManager.find(Customer.class, customer.getId());
+        assertThat(afterCommit, nullValue());
+    }
+}
