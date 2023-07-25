b81a452c11755595cb3cd2cabb3ca2d72162ab0f
Hongseob Lee <sjun2918@naver.com>
2023-07-25 23:33:07 +0900

    test : Order / OrderItem 생성 테스트

---
+++ src/test/java/com/example/springbootjpa/domain/order/OrderItemTest.java
@@ -0,0 +1,60 @@
+package com.example.springbootjpa.domain.order;
+
+import com.example.springbootjpa.domain.item.Item;
+import com.example.springbootjpa.domain.item.Mouse;
+import com.example.springbootjpa.golbal.exception.InvalidDomainConditionException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class OrderItemTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -2, -3})
+    @DisplayName("생성 실패 - orderPrice가 0보다 작다.")
+    void createOrderItemFailTest_orderPrice(int invalidOrderPrice) throws Exception {
+
+        //given
+        int count = 5;
+
+        //when -> then
+        assertThrows(InvalidDomainConditionException.class,
+                () -> new OrderItem(invalidOrderPrice, count));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -2, -3})
+    @DisplayName("생성 실패 - count가 0보다 작다.")
+    void createOrderItemFailTest_count(int invalidCount) throws Exception {
+
+        //given
+        int orderPrice = 500;
+
+        //when -> then
+        assertThrows(InvalidDomainConditionException.class,
+                () -> new OrderItem(orderPrice, invalidCount));
+    }
+
+    @Test
+    @DisplayName("OrderItem 생성 성공")
+    void createOrderItemTest() throws Exception {
+
+        //given
+        int orderPrice = 1000;
+        int quantity = 3;
+        Item item = new Mouse(100, 10, "red");
+
+        //when
+        OrderItem orderItem = OrderItem.create(item, orderPrice, quantity);
+
+        //then
+        assertThat(orderItem.getOrderPrice()).isEqualTo(orderPrice);
+        assertThat(orderItem.getQuantity()).isEqualTo(quantity);
+        assertThat(orderItem.getItem()).isEqualTo(item);
+        assertThat(item.getStockQuantity()).isEqualTo(7);
+    }
+}
---
+++ src/test/java/com/example/springbootjpa/domain/order/OrderTest.java
@@ -0,0 +1,44 @@
+package com.example.springbootjpa.domain.order;
+
+import com.example.springbootjpa.domain.customer.Customer;
+import com.example.springbootjpa.domain.item.Item;
+import com.example.springbootjpa.domain.item.Keyboard;
+import com.example.springbootjpa.domain.item.Mouse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class OrderTest {
+
+    @Test
+    @DisplayName("Order 생성 테스트")
+    void createOrderTest() throws Exception {
+
+        //given
+        Customer customer = Customer.builder()
+                .id(1L)
+                .username("hong")
+                .address("부산시")
+                .build();
+
+        Item item1 = new Keyboard(1000, 10, "white");
+        Item item2 = new Mouse(500, 5, "red");
+        int quantity1 = 3;
+        int quantity2 = 2;
+
+        OrderItem orderItem1 = OrderItem.create(item1, item1.getPrice(), quantity1);
+        OrderItem orderItem2 = OrderItem.create(item2, item2.getPrice(), quantity2);
+
+        //when
+        Order order = Order.createOrder(customer, orderItem1, orderItem2);
+
+        //then
+        assertThat(order.getCustomer()).isEqualTo(customer);
+        assertThat(order.getOrderItems()).hasSize(2);
+        assertThat(order.getOrderItems().get(0)).isEqualTo(orderItem1);
+        assertThat(order.getOrderStatus()).isEqualTo(OrderStatus.ORDER);
+        assertNotNull(order.getOrderDate());
+    }
+}
