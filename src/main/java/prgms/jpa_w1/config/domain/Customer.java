ce3d89490e385606022c3248dabda90c95c8d4be
2jie0516 <2jie0516>
2023-07-26 04:36:41 +0900

    config 파일 및 customer 도메인 생성

---
+++ src/main/java/prgms/jpa_w1/config/domain/Customer.java
@@ -0,0 +1,40 @@
+package prgms.jpa_practice.config.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @Column(name = "id")
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
