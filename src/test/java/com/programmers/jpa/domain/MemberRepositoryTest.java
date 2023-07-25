c1ad4713e4deb3c9154b6b9449b936b55f63db17
hseong3243 <hseong3243@gmail.com>
2023-07-25 17:44:26 +0900

    remove: 사용하지 않는 import 제거

--- src/test/java/com/programmers/jpa/domain/MemberRepositoryTest.java
+++ src/test/java/com/programmers/jpa/domain/MemberRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.programmers.jpa.domain;
 
 import jakarta.persistence.EntityManager;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class MemberRepositoryTest {
