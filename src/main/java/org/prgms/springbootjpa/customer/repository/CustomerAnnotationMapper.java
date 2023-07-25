3b2c2632e9136a00be6d5c04c0e15a6ceda247cb
hyeon-z <phj8221@naver.com>
2023-07-25 13:07:30 +0900

    feat: MyBatis의 Annotation을 활용한 Mapper interface 구현

---
+++ src/main/java/org/prgms/springbootjpa/customer/repository/CustomerAnnotationMapper.java
@@ -0,0 +1,26 @@
+package org.prgms.springbootjpa.customer.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.prgms.springbootjpa.customer.domain.Customer;
+
+import java.util.List;
+
+@Mapper
+public interface CustomerAnnotationMapper {
+    @Update("INSERT INTO customers (id, first_name, last_name) VALUES(#{id}, #{firstName}, #{lastName})")
+    void save(Customer customer);
+
+    @Select("SELECT * FROM customers WHERE id = #{id}")
+    Customer findById(long id);
+
+    @Select("SELECT * FROM customers")
+    List<Customer> findAll();
+
+    @Update("UPDATE customers SET first_name = #{firstName}, last_name = #{lastName} WHERE id = #{id}")
+    void update(Customer customer);
+
+    @Update("DELETE FROM customers WHERE id = #{id}")
+    void deleteById(long id);
+}
