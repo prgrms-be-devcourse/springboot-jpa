e84081df5aa91ca530093c418b45aebecc1c9b83
sujjangOvO <pnk7038@gmail.com>
2023-07-26 11:32:54 +0900

    Customer 도메인 Name 검증, 생성자 구현

--- src/main/java/com/example/kdtjpa/domain/Customer.java
+++ src/main/java/com/example/kdtjpa/domain/Customer.java
@@ -1,5 +1,8 @@
 package com.example.kdtjpa.domain;
 
+import java.util.InputMismatchException;
+import java.util.regex.Pattern;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -11,6 +14,25 @@ public class Customer {
 	private long id;
 	private String firstName;
 	private String lastName;
+
+	public Customer(long id, String firstName, String lastName) {
+		this.id = id;
+		this.firstName = validateName(firstName);
+		this.lastName = validateName(lastName);
+	}
+
+	public Customer() {
+	}
+
+	private String validateName(String name) {
+		boolean validator = Pattern.matches("^[a-zA-Z\\d]+", name);
+
+		if(validator) {
+			return name;
+		}
+
+		throw new InputMismatchException("Not validatedName");
+	}
 
 	public long getId() {
 		return id;
