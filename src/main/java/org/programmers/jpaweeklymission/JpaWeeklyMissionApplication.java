8885c72e210cca4e1749ce564df73fddb2802afa
sangminnim <sangmins930@gmail.com>
2023-07-26 23:42:26 +0900

    feat: initial commit

---
+++ src/main/java/org/programmers/jpaweeklymission/JpaWeeklyMissionApplication.java
@@ -0,0 +1,13 @@
+package org.programmers.jpaweeklymission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWeeklyMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaWeeklyMissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/programmers/jpaweeklymission/JpaWeeklyMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programmers.jpaweeklymission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWeeklyMissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
