ab3d743b23d61fb977a3c6f297b668fedf49aa5c
sangminnim <sangmins930@gmail.com>
2023-07-27 05:06:39 +0900

    feat: BaseEntity 생성

--- src/main/java/org/programmers/jpaweeklymission/JpaWeeklyMissionApplication.java
+++ src/main/java/org/programmers/jpaweeklymission/JpaWeeklyMissionApplication.java
@@ -2,8 +2,10 @@ package org.programmers.jpaweeklymission;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JpaWeeklyMissionApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/programmers/jpaweeklymission/global/BaseEntity.java
@@ -0,0 +1,18 @@
+package org.programmers.jpaweeklymission.global;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    LocalDateTime createAt;
+    @LastModifiedDate
+    LocalDateTime updateAt;
+}
