23159030453a7e5773f6eddd18e1c03a15bc3a79
hi-june <wnsgur735@gmail.com>
2023-07-26 12:32:32 +0900

    feat: customer repository 추가

---
+++ jpastudy/src/main/java/com/programmers/june/jpastudy/domain/customer/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.programmers.june.jpastudy.domain.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
