a6820af2e9a870d269c9a53f7d7b5552bb63d63f
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-07-27 01:45:34 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/programmers/jpamission/JpaMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.jpamission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
