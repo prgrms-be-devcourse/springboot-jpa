2db08f4c1617eaaaf82b80c7287b16bdd9e59489
HandmadeCloud <joje1003@gmail.com>
2023-07-26 15:51:35 +0900

    feat 영속성컨텍스트 실습

--- src/main/java/com/example/weeklyjpa/domain/order/Order.java
+++ src/main/java/com/example/weeklyjpa/domain/order/Order.java
@@ -1,4 +1,15 @@
 package com.example.weeklyjpa.domain.order;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Orders")
+@NoArgsConstructor
 public class Order {
+    @Id
+    private Long id;
+
 }
