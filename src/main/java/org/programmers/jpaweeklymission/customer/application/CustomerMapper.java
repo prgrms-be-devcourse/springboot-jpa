926e69ed6518a82f848d6f81edd1410d72f23184
sangminnim <sangmins930@gmail.com>
2023-07-27 05:09:49 +0900

    feat: CustomerMapper 생성

---
+++ src/main/java/org/programmers/jpaweeklymission/customer/application/CustomerMapper.java
@@ -0,0 +1,26 @@
+package org.programmers.jpaweeklymission.customer.application;
+
+import org.programmers.jpaweeklymission.customer.presentation.dto.CustomerCreationRequest;
+import org.programmers.jpaweeklymission.customer.application.dto.CustomerResponse;
+import org.programmers.jpaweeklymission.customer.data.Customer;
+
+public final class CustomerMapper {
+    private CustomerMapper() {
+
+    }
+
+    public static CustomerResponse toResponse(Customer customer) {
+        return CustomerResponse.builder()
+                .id(customer.getId())
+                .firstName(customer.getFirstName())
+                .lastName(customer.getLastName())
+                .build();
+    }
+
+    public static Customer toCustomer(CustomerCreationRequest request) {
+        return Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .build();
+    }
+}
