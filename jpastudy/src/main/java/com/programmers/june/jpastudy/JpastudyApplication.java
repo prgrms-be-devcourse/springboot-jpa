9e39248c480e03b65aa0e0f85ac7751251c73977
hi-june <wnsgur735@gmail.com>
2023-07-26 10:30:40 +0900

    chore: JPA setting

---
+++ jpastudy/src/main/java/com/programmers/june/jpastudy/JpastudyApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.june.jpastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpastudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpastudyApplication.class, args);
+	}
+
+}
