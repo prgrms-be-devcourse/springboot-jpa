56150de1956992014566d150da6bf94af8fe0ec7
sangminnim <sangmins930@gmail.com>
2023-07-27 05:10:17 +0900

    feat: Customer Service 구현

---
+++ src/main/java/org/programmers/jpaweeklymission/customer/application/CustomerService.java
@@ -0,0 +1,46 @@
+package org.programmers.jpaweeklymission.customer.application;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.programmers.jpaweeklymission.customer.presentation.dto.CustomerCreationRequest;
+import org.programmers.jpaweeklymission.customer.data.Customer;
+import org.programmers.jpaweeklymission.customer.data.CustomerJpaRepository;
+import org.programmers.jpaweeklymission.customer.application.dto.CustomerResponse;
+import org.programmers.jpaweeklymission.customer.presentation.dto.CustomerUpdateRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+    private static final String CUSTOMER_NOT_FOUND = "Customer not found for that id: %s";
+    private final CustomerJpaRepository customerJpaRepository;
+
+    @Transactional
+    public CustomerResponse saveCustomer(CustomerCreationRequest request) {
+        Customer customer = CustomerMapper.toCustomer(request);
+        Customer saved = customerJpaRepository.save(customer);
+        return CustomerMapper.toResponse(saved);
+    }
+
+    @Transactional
+    public CustomerResponse updateCustomer(Long id, CustomerUpdateRequest request) {
+        Customer customer = customerJpaRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(String.format(CUSTOMER_NOT_FOUND, id))
+        );
+        customer.changeFirstName(request.firstName());
+        customer.changeLastName(request.lastName());
+        return CustomerMapper.toResponse(customer);
+    }
+
+    @Transactional
+    public void deleteCustomer(Long id) {
+        customerJpaRepository.deleteById(id);
+    }
+
+    public CustomerResponse findCustomerById(Long id) {
+        Customer customer = customerJpaRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.format(CUSTOMER_NOT_FOUND, id)));
+        return CustomerMapper.toResponse(customer);
+    }
+}
