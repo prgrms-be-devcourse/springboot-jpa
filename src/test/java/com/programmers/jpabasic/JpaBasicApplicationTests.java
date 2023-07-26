98c004deddfb0e36b018d76354d90ce57950f475
kmebin <blowhuiee@gmail.com>
2023-07-26 09:43:26 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/programmers/jpabasic/JpaBasicApplicationTests.java
@@ -0,0 +1,12 @@
+package com.programmers.jpabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
