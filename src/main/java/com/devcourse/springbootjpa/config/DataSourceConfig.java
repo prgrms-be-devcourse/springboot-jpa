ac591eb0ada818bf2fd80dd72d3ebefb46b00de4
SW-H <hsw08@naver.com>
2023-07-26 17:37:02 +0900

    feat : CRUD 구현, 테스트 구현

--- src/main/java/com/devcourse/springbootjpa/config/DataSourceConfig.java
+++ src/main/java/com/devcourse/springbootjpa/config/DataSourceConfig.java
@@ -17,47 +17,47 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class DataSourceConfig {
-	// @Bean
-	// public DataSource dataSource() {
-	// 	DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	// 	dataSource.setDriverClassName("org.h2.Driver");
-	// 	dataSource.setUrl("jdbc:h2:~/test");
-	// 	dataSource.setUsername("sa");
-	// 	dataSource.setPassword("");
-	//
-	// 	return dataSource;
-	// }
-	//
-	// @Bean
-	// public JpaVendorAdapter jpaVendorAdapter(JpaProperties jpaProperties) {
-	// 	AbstractJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-	// 	adapter.setShowSql(jpaProperties.isShowSql());
-	// 	adapter.setDatabasePlatform(jpaProperties.getDatabasePlatform());
-	// 	adapter.setGenerateDdl(jpaProperties.isGenerateDdl());
-	// 	return adapter;
-	// }
-	//
-	// @Bean
-	// public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter,
-	// 		JpaProperties jpaProperties) {
-	// 	LocalContainerEntityManagerFactoryBean em
-	// 			= new LocalContainerEntityManagerFactoryBean();
-	// 	em.setDataSource(dataSource);
-	// 	em.setPackagesToScan("com.devcourse.springbootjpa.domain");
-	// 	em.setJpaVendorAdapter(jpaVendorAdapter);
-	//
-	// 	Properties properties = new Properties();
-	// 	properties.putAll(jpaProperties.getProperties());
-	// 	em.setJpaProperties(properties);
-	//
-	// 	return em;
-	// }
-	//
-	// @Bean
-	// public PlatformTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
-	// 	JpaTransactionManager transactionManager = new JpaTransactionManager();
-	// 	transactionManager.setEntityManagerFactory(entityManagerFactory.getObject());
-	//
-	// 	return transactionManager;
-	// }
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:~/test");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+
+		return dataSource;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter(JpaProperties jpaProperties) {
+		AbstractJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setShowSql(jpaProperties.isShowSql());
+		adapter.setDatabasePlatform(jpaProperties.getDatabasePlatform());
+		adapter.setGenerateDdl(jpaProperties.isGenerateDdl());
+		return adapter;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter,
+			JpaProperties jpaProperties) {
+		LocalContainerEntityManagerFactoryBean em
+				= new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource);
+		em.setPackagesToScan("com.devcourse.springbootjpa.domain");
+		em.setJpaVendorAdapter(jpaVendorAdapter);
+
+		Properties properties = new Properties();
+		properties.putAll(jpaProperties.getProperties());
+		em.setJpaProperties(properties);
+
+		return em;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory.getObject());
+
+		return transactionManager;
+	}
 }
