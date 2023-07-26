942417d0a1a0b3efaadea37fb599faa8302bfcdf
hseong3243 <hseong3243@gmail.com>
2023-07-26 14:31:36 +0900

    feat: Member 생성, 업데이트 시 입력값에 대한 검증 추가

--- src/main/java/com/programmers/jpa/domain/Member.java
+++ src/main/java/com/programmers/jpa/domain/Member.java
@@ -3,9 +3,13 @@ package com.programmers.jpa.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.util.regex.Pattern;
+
 @Getter
 @Entity
 public class Member {
+    private static final Pattern NAME_PATTERN = Pattern.compile("[A-Za-z]{1,20}$");
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
@@ -19,16 +23,37 @@ public class Member {
     }
 
     public Member(String firstName, String lastName) {
+        validateFirstName(firstName);
+        validateFirstName(lastName);
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
     public void update(String firstName, String lastName) {
         if(firstName != null) {
+            validateFirstName(firstName);
             this.firstName = firstName;
         }
         if(lastName != null) {
+            validateLastName(lastName);
             this.lastName = lastName;
         }
+    }
+
+    private void validateFirstName(String firstName) {
+        if(invalidName(firstName)) {
+            throw new IllegalArgumentException("Invalid first name");
+        }
+    }
+
+    private void validateLastName(String lastName) {
+        if(invalidName(lastName)) {
+            throw new IllegalArgumentException("Invalid last Name");
+        }
+    }
+
+    private boolean invalidName(String firstName) {
+        boolean valid = NAME_PATTERN.matcher(firstName).matches();
+        return !valid;
     }
 }
