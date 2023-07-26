9bec2186410d0345ef2066177e7fa65a178f03ab
sangminnim <sangmins930@gmail.com>
2023-07-27 05:08:15 +0900

    feat: Customer repository 생성

---
+++ src/main/java/org/programmers/jpaweeklymission/customer/data/CustomerJpaRepository.java
@@ -0,0 +1,6 @@
+package org.programmers.jpaweeklymission.customer.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerJpaRepository extends JpaRepository<Customer, Long> {
+}
