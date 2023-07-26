a2f479944cabf746cc5998c78ce56dbd73925a73
heenahan <mby1997s@gmail.com>
2023-07-26 14:50:37 +0900

    feat : mission 2. 영속성컨텍스트(customer 엔티티를 이용하여 생명주기 실습)

--- src/test/java/com/prgms/springbootjpa/mission1/JpaIntroduce.java
+++ src/test/java/com/prgms/springbootjpa/mission1/JpaIntroduce.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
@@ -33,7 +32,7 @@ public class JpaIntroduce {
     }
 
     @Test
-    @DisplayName("reade - customer 조회한다.")
+    @DisplayName("read - customer 조회한다.")
     void 조회() {
 
         //given
