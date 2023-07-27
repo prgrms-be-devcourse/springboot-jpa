f8bad88ca10e93dc6be58a468c376a92981ae146
bumwoo <gld55602@gmail.com>
2023-07-27 23:52:25 +0900

    feat: Member Id GenerationType 변경

--- src/main/java/com/lecture/jpa/domain/order/Member.java
+++ src/main/java/com/lecture/jpa/domain/order/Member.java
@@ -18,7 +18,7 @@ import lombok.Setter;
 @Setter
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name", nullable = false, length = 30)
