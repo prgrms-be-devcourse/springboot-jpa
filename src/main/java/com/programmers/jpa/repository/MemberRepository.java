0df6ed74e8aace151c40d1dfd560d7ea7e153b30
hseong3243 <hseong3243@gmail.com>
2023-07-27 09:43:27 +0900

    move: repository 패키지 이동

--- src/main/java/com/programmers/jpa/repository/MemberRepository.java
+++ src/main/java/com/programmers/jpa/repository/MemberRepository.java
@@ -1,5 +1,6 @@
-package com.programmers.jpa.domain;
+package com.programmers.jpa.repository;
 
+import com.programmers.jpa.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
