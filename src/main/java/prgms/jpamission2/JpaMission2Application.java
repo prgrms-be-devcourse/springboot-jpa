d16e4a6c91d209b3ada3ffe0927922f021fc4930
2jie0516 <2jie0516>
2023-07-27 17:27:59 +0900

    영속성 컨텍스트 테스토 코드 작성 커밋

---
+++ src/main/java/prgms/jpamission2/JpaMission2Application.java
@@ -0,0 +1,13 @@
+package prgms.jpamission2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMission2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMission2Application.class, args);
+    }
+
+}
---
+++ src/main/java/prgms/jpamission2/config/domain/CustomerRepository.java
@@ -0,0 +1,6 @@
+package prgms.jpamission2.config.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+}
