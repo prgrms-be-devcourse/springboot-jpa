524cb03c83e5f724d85157ea757985a53145ec4d
2jie0516 <2jie0516>
2023-07-27 14:02:56 +0900

    테스트 시작 전 BeforeAll에서 리스트 초기화 진행 후 사용

--- src/test/java/prgms/jpa_w1/domain/CustomerRepositoryTest.java
+++ src/test/java/prgms/jpa_w1/domain/CustomerRepositoryTest.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,22 @@ class CustomerRepositoryTest {
     @Autowired
     CustomerRepository customerRepository;
 
+    private static List<Customer> customerList;
+
+    @BeforeAll
+    static void beforeAll() {
+        Customer customer1 = new Customer();
+        customer1.setId(1L);
+        customer1.setFirstName("oh");
+        customer1.setLastName("sehan");
+
+        Customer customer2 = new Customer();
+        customer2.setId(2L);
+        customer2.setFirstName("five");
+        customer2.setLastName("hanse");
+
+        customerList = List.of(customer1, customer2);
+    }
     @Nested
     class 엔티티_저장_테스트 {
         @Test
@@ -96,17 +113,7 @@ class CustomerRepositoryTest {
         @Test
         void 리스트조회_확인() {
             // Given
-            Customer customer1 = new Customer();
-            customer1.setId(1L);
-            customer1.setFirstName("honggu");
-            customer1.setLastName("kang");
-
-            Customer customer2 = new Customer();
-            customer2.setId(2L);
-            customer2.setFirstName("guppy");
-            customer2.setLastName("hong");
-
-            customerRepository.saveAll(Lists.newArrayList(customer1, customer2));
+            customerRepository.saveAll(customerList);
 
             // When
             List<Customer> selectedCustomers = customerRepository.findAll();
@@ -141,11 +148,7 @@ class CustomerRepositoryTest {
         @Test
         void 모든_엔티티_삭제() {
             // Given
-            Customer customer = new Customer();
-            customer.setId(1L);
-            customer.setFirstName("honggu");
-            customer.setLastName("kang");
-            Customer entity = customerRepository.save(customer);
+            customerRepository.saveAll(customerList);
 
             // When
             customerRepository.deleteAll();
@@ -168,7 +171,7 @@ class CustomerRepositoryTest {
             customer2.setFirstName("honggu");
             customer2.setLastName("kang");
 
-            customerRepository.saveAll(Lists.newArrayList(customer1, customer2));
+            customerRepository.saveAll(customerList);
 
             // When
             customerRepository.delete(customer1);
