752582be57a625a09156e6fa54f674f03f486bbd
hi-june <wnsgur735@gmail.com>
2023-07-27 16:48:18 +0900

    feat: customer 이름에 관련된 column 제약 조건 추가

--- jpastudy/src/main/java/com/programmers/june/jpastudy/domain/customer/Customer.java
+++ jpastudy/src/main/java/com/programmers/june/jpastudy/domain/customer/Customer.java
@@ -14,7 +14,11 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
 
     @Builder
