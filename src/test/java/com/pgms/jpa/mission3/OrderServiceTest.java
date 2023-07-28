fe6553de6e87aa8cba6f0243c80ddc2d0763e4db
Bombo <bombo96@chungwoon.ac.kr>
2023-07-28 16:43:20 +0900

    test : 연관관계 비즈니스 테스트

--- src/test/java/com/pgms/jpa/mission3/OrderServiceTest.java
+++ src/test/java/com/pgms/jpa/mission3/OrderServiceTest.java
@@ -1,8 +1,10 @@
-package com.pgms.jpa.domain.order.service;
+package com.pgms.jpa.mission3;
 
 import com.pgms.jpa.domain.item.Item;
 import com.pgms.jpa.domain.item.ItemRepository;
 import com.pgms.jpa.domain.order.Order;
+import com.pgms.jpa.domain.order.OrderStatus;
+import com.pgms.jpa.domain.order.service.OrderService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,4 +40,22 @@ class OrderServiceTest {
         Assertions.assertThat(findOrder.getCreatedAt()).isNotNull();
     }
 
+    @Test
+    @Transactional
+    @DisplayName("주문을 취소할 수 있다.")
+    void cancelOrderTest() {
+        //given
+        Item item = new Item("phone", 10000, 3);
+        itemRepository.save(item);
+
+        Long orderId = orderService.createOrder(item.getId(), 2);
+        Order findOrder = orderService.findOne(orderId);
+
+        //when
+        findOrder.cancelOrder();
+
+        //then
+        Assertions.assertThat(findOrder.getOrderStatus()).isEqualTo(OrderStatus.CANCEL);
+        Assertions.assertThat(item.getQuantity()).isEqualTo(3);
+    }
 }
