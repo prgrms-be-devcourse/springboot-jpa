b015a6e6eeaddc5787e8b482ae2f29f20371a689
hyeon-z <phj8221@naver.com>
2023-07-28 13:52:15 +0900

    feat: Item 엔티티 구현

---
+++ src/main/java/org/prgms/springbootjpa/mission3/domain/order/Item.java
@@ -0,0 +1,38 @@
+package org.prgms.springbootjpa.mission3.domain.order;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private int price;
+
+    private int stockQuantity;
+
+    @OneToMany(mappedBy = "item")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    public Item(int price, int stockQuantity) {
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+    }
+}
