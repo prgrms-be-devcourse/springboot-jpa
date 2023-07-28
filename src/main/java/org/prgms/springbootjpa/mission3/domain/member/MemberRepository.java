0461eeca24e2395e325db0d3acc654cb21873f82
hyeon-z <phj8221@naver.com>
2023-07-28 10:47:54 +0900

    feat: JpaRepository를 상속하는 Member, Order Repository 생성

---
+++ src/main/java/org/prgms/springbootjpa/mission3/domain/member/MemberRepository.java
@@ -0,0 +1,8 @@
+package org.prgms.springbootjpa.mission3.domain.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
