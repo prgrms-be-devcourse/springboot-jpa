77b7e85aa2a97c1a6f3d34255e0ee7d3b3f87887
2jie0516 <2jie0516>
2023-07-29 03:12:27 +0900

    테스트 케이스 초기 데이터 설정

--- src/test/java/prgms/mission3/order/OrderRepositoryTest.java
+++ src/test/java/prgms/mission3/order/OrderRepositoryTest.java
@@ -1,7 +1,6 @@
 package prgms.mission3.order;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,50 +10,72 @@ import prgms.mission3.order.domain.Order;
 import prgms.mission3.order.domain.OrderStatus;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 
 @SpringBootTest
+@Transactional
 public class OrderRepositoryTest {
-
-    String uuid = UUID.randomUUID().toString();
 
     @Autowired
     OrderRepository orderRepository;
 
-    @BeforeEach
+    private static Order order1;
+    private static Order order2;
+
+    @BeforeAll
     void setUp () {
-        Member member = new Member();
-        member.setName("강홍구");
-        member.setAge(33);
-        member.setNickName("guppy.kang");
-        member.setAddress("서울시 동작구만 움직이면 쏜다.");
-        member.setDescription("");
+        Member member1 = new Member();
+        member1.setName("오세한");
+        member1.setAge(27);
+        member1.setNickName("sehan");
+        member1.setAddress("경기도 용인시");
+        member1.setDescription("백둥이");
 
-        Order order = new Order();
-        order.setUuid(uuid);
-        order.setMemo("부재시 전화주세요.");
-        order.setOrderDatetime(LocalDateTime.now());
-        order.setOrderStatus(OrderStatus.OPENED);
+        Member member2 = new Member();
+        member2.setName("세한오");
+        member2.setAge(27);
+        member2.setNickName("hanse");
+        member2.setAddress("충북 청주시");
+        member2.setDescription("프롱이");
 
-        order.setMember(member);
+        Order order1 = new Order();
+        order1.setUuid(UUID.randomUUID().toString());
+        order1.setMemo("부재시 전화주세요.");
+        order1.setOrderDatetime(LocalDateTime.now());
+        order1.setOrderStatus(OrderStatus.OPENED);
 
-        orderRepository.save(order); // INSERT
-    }
+        order1.setMember(member1);
 
-    @AfterEach
-    void tearDown() {
-        orderRepository.deleteAll();
+        Order order2 = new Order();
+        order2.setUuid(UUID.randomUUID().toString());
+        order2.setMemo("문 앞에 놔주세요");
+        order2.setOrderDatetime(LocalDateTime.now());
+        order2.setOrderStatus(OrderStatus.CANCELLED);
+
+        order2.setMember(member2);
+
+        orderRepository.save(order1);
+        orderRepository.save(order2);
     }
 
     @Test
-    @Transactional
-    void JPA_query() {
-        Order order = orderRepository.findById(uuid).get(); // SELECT * FROM orders WHERE id = ?
-        List<Order> all = orderRepository.findAll(); // SELECT * FROM orders
-        orderRepository.existsById(uuid);
+    void 주문_조회_테스트() {
+        
+
     }
+
+    @Test
+    void 회원_조회_테스트() {
+
+    }
+
+    @Test
+    void 연관관계_메소드_테스트() {
+
+    }
+
+
 
 
 
