64202f76b7f59a4420a6482b5c860b996e65de4f
Juwoong <wwong7247@gmail.com>
2023-07-28 16:27:07 +0900

    CustomerRepository로 네이밍 수정

--- src/main/java/com/example/springbootjpa/customer/repository/CustomerRepository.java
+++ src/main/java/com/example/springbootjpa/customer/repository/CustomerRepository.java
@@ -3,5 +3,5 @@ package com.example.springbootjpa.customer.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.springbootjpa.customer.model.Customer;
 
-public interface JpaCustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
--- src/test/java/com/example/springbootjpa/customer/repository/CustomerRepositoryTest.java
+++ src/test/java/com/example/springbootjpa/customer/repository/CustomerRepositoryTest.java
@@ -10,10 +10,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import com.example.springbootjpa.customer.model.Customer;
 
 @DataJpaTest
-class JpaCustomerRepositoryTest {
+class CustomerRepositoryTest {
 
     @Autowired
-    private JpaCustomerRepository jpaCustomerRepository;
+    private CustomerRepository customerRepository;
 
     @Test
     @DisplayName("저장 기능 테스트")
@@ -22,7 +22,7 @@ class JpaCustomerRepositoryTest {
         Customer customer = new Customer(1L, "firstName", "lastName");
 
         // when
-        Customer savedCustomer = jpaCustomerRepository.save(customer);
+        Customer savedCustomer = customerRepository.save(customer);
 
         // then
         assertEquals("firstName", savedCustomer.getFirstName());
@@ -33,10 +33,10 @@ class JpaCustomerRepositoryTest {
     void findByIdTest() {
         // given
         Customer customer = new Customer(1L, "firstName", "lastName");
-        jpaCustomerRepository.save(customer);
+        customerRepository.save(customer);
 
         // when
-        Optional<Customer> selectedCustomer = jpaCustomerRepository.findById(1L);
+        Optional<Customer> selectedCustomer = customerRepository.findById(1L);
 
         // then
         assertEquals("firstName", selectedCustomer.get().getFirstName());
@@ -49,11 +49,11 @@ class JpaCustomerRepositoryTest {
         Customer customer1 = new Customer(1L, "firstFirstName", "firstLastName");
         Customer customer2 = new Customer(2L, "secondFirstName", "secondLastName");
 
-        jpaCustomerRepository.save(customer1);
-        jpaCustomerRepository.save(customer2);
+        customerRepository.save(customer1);
+        customerRepository.save(customer2);
 
         // when
-        List<Customer> selectedCustomer = jpaCustomerRepository.findAll();
+        List<Customer> selectedCustomer = customerRepository.findAll();
 
         // then
         assertEquals(2, selectedCustomer.size());
@@ -64,15 +64,15 @@ class JpaCustomerRepositoryTest {
     void JpaCustomerRepositoryTest() {
         // given
         Customer customer = new Customer(1L, "firstName", "lastName");
-        jpaCustomerRepository.save(customer);
+        customerRepository.save(customer);
 
-        customer = jpaCustomerRepository.findById(1L).get();
+        customer = customerRepository.findById(1L).get();
         customer.setFirstName("editedFirstName");
         customer.setLastName("editedLastName");
-        jpaCustomerRepository.save(customer);
+        customerRepository.save(customer);
 
         // when
-        customer = jpaCustomerRepository.findById(1L).get();
+        customer = customerRepository.findById(1L).get();
 
         // then
         assertEquals("editedFirstName", customer.getFirstName());
@@ -83,14 +83,13 @@ class JpaCustomerRepositoryTest {
     void deleteTest() {
         // given
         Customer customer = new Customer(1L, "firstName","lastName");
-        customer = jpaCustomerRepository.save(customer);
-        assertTrue(jpaCustomerRepository.findById(1L).isPresent());
+        assertTrue(customerRepository.findById(1L).isPresent());
 
         // when
-        jpaCustomerRepository.deleteById(1L);
+        customerRepository.deleteById(1L);
 
         // then
-        assertFalse(jpaCustomerRepository.findById(1L).isPresent());
+        assertFalse(customerRepository.findById(1L).isPresent());
     }
 
 
