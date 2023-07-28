6139ea817c24575a16db1b5382e0b0bdb81978f4
Juwoong <wwong7247@gmail.com>
2023-07-28 16:10:07 +0900

    Customer 엔티티 생성

---
+++ src/main/java/com/example/springbootjpa/customer/model/Customer.java
@@ -0,0 +1,24 @@
+package com.example.springbootjpa.customer.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id // Id PK
+    private long id;
+    private String firstName;
+    private String lastName;
+
+}
