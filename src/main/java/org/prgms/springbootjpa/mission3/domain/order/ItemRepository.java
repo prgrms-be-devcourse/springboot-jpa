ababb8b1ec5c7918dd9240fa3f5897a7560e0ce9
hyeon-z <phj8221@naver.com>
2023-07-28 13:52:49 +0900

    feat: feat: JpaRepository를 상속하는 ItemRepository 생성

---
+++ src/main/java/org/prgms/springbootjpa/mission3/domain/order/ItemRepository.java
@@ -0,0 +1,8 @@
+package org.prgms.springbootjpa.mission3.domain.order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
