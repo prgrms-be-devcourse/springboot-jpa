54cb8bdc56fea5b88eaab2393b5903ee06e3eda5
hyeon-z <phj8221@naver.com>
2023-07-29 11:49:54 +0900

    refactor: @Repository 어노테이션 제거

--- src/main/java/org/prgms/springbootjpa/mission3/domain/member/MemberRepository.java
+++ src/main/java/org/prgms/springbootjpa/mission3/domain/member/MemberRepository.java
@@ -3,6 +3,5 @@ package org.prgms.springbootjpa.mission3.domain.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
--- src/main/java/org/prgms/springbootjpa/mission3/domain/order/OrderItemRepository.java
+++ src/main/java/org/prgms/springbootjpa/mission3/domain/order/OrderItemRepository.java
@@ -1,8 +1,6 @@
 package org.prgms.springbootjpa.mission3.domain.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
