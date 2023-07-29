510c77c94ba7615a6211b74d32ea30cc94313b22
yumyeonghan <mgtmh991013@gmail.com>
2023-07-29 10:28:12 +0900

    feat: Item 엔티티 재설계

--- src/main/java/com/programmers/jpa/item/domain/Item.java
+++ src/main/java/com/programmers/jpa/item/domain/Item.java
@@ -2,11 +2,16 @@ package com.programmers.jpa.item.domain;
 
 import com.programmers.jpa.base.domain.BaseEntity;
 import jakarta.persistence.*;
+import org.hibernate.annotations.Formula;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "item_type")
 public abstract class Item extends BaseEntity {
+
+    private static final String FOOD = "FOOD";
+    private static final String CAR = "CAR";
+    private static final int MIN = 0;
 
     @Id
     @GeneratedValue
@@ -14,7 +19,8 @@ public abstract class Item extends BaseEntity {
     private int price;
     private int stockQuantity;
 
-    @Column(name = "item_type", insertable = false, updatable = false, nullable = false)
+    @Formula("(SELECT item_type FROM item WHERE id = id)")
+    @Column(name = "item_type", insertable = false, updatable = false)
     private String itemType;
 
     protected Item() {
@@ -28,27 +34,27 @@ public abstract class Item extends BaseEntity {
     }
     
     private static void validatePrice(int price) {
-        if (price < 0) {
+        if (price < MIN) {
             throw new IllegalArgumentException(String.format("가격이 음수입니다. input: %s", price));
         }
     }
 
     private static void validateStockQuantity(int stockQuantity) {
-        if (stockQuantity < 0) {
+        if (stockQuantity < MIN) {
             throw new IllegalArgumentException(String.format("재고수량이 음수입니다. input: %s", stockQuantity));
         }
     }
 
     public String getChef() {
-        if (!this.itemType.equals("FOOD")) {
-            throw new IllegalArgumentException(String.format("해당 상품은 음식이 아닙니다."));
+        if (!this.itemType.equals(FOOD)) {
+            throw new IllegalArgumentException("해당 상품은 음식이 아닙니다.");
         }
         return null;
     }
 
     public Long getPower() {
-        if (!this.itemType.equals("CAR")) {
-            throw new IllegalArgumentException(String.format("해당 상품은 자동차가 아닙니다."));
+        if (!this.itemType.equals(CAR)) {
+            throw new IllegalArgumentException("해당 상품은 자동차가 아닙니다.");
         }
         return null;
     }
