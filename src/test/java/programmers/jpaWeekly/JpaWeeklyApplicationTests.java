e62dbaf7babd4b3798130ca5ee3aeb6f8667fb87
kys0411 <32180846@dankook.ac.kr>
2023-07-29 21:25:05 +0900

    프로젝트 초기화

---
+++ src/test/java/programmers/jpaWeekly/JpaWeeklyApplicationTests.java
@@ -0,0 +1,13 @@
+package programmers.jpaWeekly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWeeklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
