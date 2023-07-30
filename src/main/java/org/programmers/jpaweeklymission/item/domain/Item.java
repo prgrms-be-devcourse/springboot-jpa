924972157a80ee1bbb4f12d2f3be0956ddee0b11
sangminnim <sangmins930@gmail.com>
2023-07-30 15:48:50 +0900

    feat: Item 구현

---
+++ src/main/java/org/programmers/jpaweeklymission/item/domain/Item.java
@@ -0,0 +1,31 @@
+package org.programmers.jpaweeklymission.item.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.programmers.jpaweeklymission.global.BaseEntity;
+
+@Table(name = "items")
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item extends BaseEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "stock_quantity")
+    private int stockQuantity;
+
+    @Builder
+    public Item(int price, int stockQuantity) {
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+}
