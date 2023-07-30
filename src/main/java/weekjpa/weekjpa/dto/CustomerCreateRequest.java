e602157b567fcc4e8356caf9a6d88c02a81bb341
jun <alap_u@naver.com>
2023-07-30 22:56:42 +0900

    CustomerDto객체생성

--- src/main/java/weekjpa/weekjpa/dto/CustomerCreateRequest.java
+++ src/main/java/weekjpa/weekjpa/dto/CustomerCreateRequest.java
@@ -4,5 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 
 public record CustomerCreateRequest(
         @NotBlank(message = "이름을 입력해 주세요.") String firstName,
-        @NotBlank(message = "성을 입력해 주세요.") String lastName) {
+        @NotBlank(message = "성을 입력해 주세요.") String lastName
+) {
 }
--- src/main/java/weekjpa/weekjpa/dto/CustomerUpdateResponse.java
+++ src/main/java/weekjpa/weekjpa/dto/CustomerUpdateResponse.java
@@ -3,7 +3,8 @@ package weekjpa.weekjpa.dto;
 import weekjpa.weekjpa.domain.Customer;
 
 public record CustomerUpdateResponse(
-        String firstName, String lastName) {
+        String firstName, String lastName
+) {
     public static CustomerUpdateResponse from(Customer customer) {
         return new CustomerUpdateResponse(customer.getFirstName(), customer.getLastName());
     }
