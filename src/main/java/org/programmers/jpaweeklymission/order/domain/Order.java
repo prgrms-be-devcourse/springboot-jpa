24f01c4abc8863d7566b01546b0d22f82eb35db1
sangminnim <sangmins930@gmail.com>
2023-07-30 15:49:10 +0900

    feat: Order 도메인 구현

---
+++ src/main/java/org/programmers/jpaweeklymission/order/domain/Order.java
@@ -0,0 +1,58 @@
+package org.programmers.jpaweeklymission.order.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.programmers.jpaweeklymission.customer.Customer;
+import org.programmers.jpaweeklymission.global.BaseEntity;
+
+import java.util.List;
+import java.util.Objects;
+
+@Table(name = "orders")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    @NotNull
+    OrderStatus status;
+
+    @Lob
+    @Column(name = "memo")
+    private String memo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+    Customer customer;
+
+    @OneToMany(mappedBy = "order")
+    List<OrderItem> orderItems;
+
+    @Builder
+    public Order(OrderStatus status, String memo) {
+        this.status = status;
+        this.memo = memo;
+    }
+
+    public void setCustomer(Customer customer) {
+        if (Objects.nonNull(this.customer)) {
+            customer.removeOrder(this);
+        }
+        this.customer = customer;
+        customer.getOrders().add(this);
+    }
+
+    public void addOrderItems(OrderItem orderItem) {
+        orderItem.setOrder(this);
+    }
+}
---
+++ src/main/java/org/programmers/jpaweeklymission/order/domain/OrderStatus.java
@@ -0,0 +1,8 @@
+package org.programmers.jpaweeklymission.order.domain;
+
+
+public enum OrderStatus {
+    CREATED,
+    SHIPPING,
+    DELIVERED
+}
