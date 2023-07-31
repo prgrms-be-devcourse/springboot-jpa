cadaf521edc3667cc1b24a24141c4ad98019e884
sangminnim <sangmins930@gmail.com>
2023-08-01 01:38:46 +0900

    feat: CustomerUpdateRequest 객체를 Customer로 변환하는 메서드 생성

--- src/main/java/org/programmers/jpaweeklymission/customer/application/CustomerMapper.java
+++ src/main/java/org/programmers/jpaweeklymission/customer/application/CustomerMapper.java
@@ -1,14 +1,14 @@
 package org.programmers.jpaweeklymission.customer.application;
 
-import org.programmers.jpaweeklymission.customer.presentation.dto.CustomerCreationRequest;
-import org.programmers.jpaweeklymission.customer.application.dto.CustomerResponse;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.programmers.jpaweeklymission.customer.Customer;
+import org.programmers.jpaweeklymission.customer.application.dto.CustomerResponse;
+import org.programmers.jpaweeklymission.customer.presentation.dto.CustomerCreationRequest;
+import org.programmers.jpaweeklymission.customer.presentation.dto.CustomerUpdateRequest;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class CustomerMapper {
-    private CustomerMapper() {
-
-    }
-
     public static CustomerResponse toResponse(Customer customer) {
         return CustomerResponse.builder()
                 .id(customer.getId())
@@ -18,6 +18,13 @@ public final class CustomerMapper {
     }
 
     public static Customer toCustomer(CustomerCreationRequest request) {
+        return Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .build();
+    }
+
+    public static Customer toCustomer(CustomerUpdateRequest request) {
         return Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
