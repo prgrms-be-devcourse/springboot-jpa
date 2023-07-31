e098e33b9bbf38b9f959013ce1b99bcdf48dbe45
sangminnim <sangmins930@gmail.com>
2023-08-01 01:40:05 +0900

    feat: 400 500 상태코드에 body 추가

--- src/main/java/org/programmers/jpaweeklymission/global/GlobalExceptionHandler.java
+++ src/main/java/org/programmers/jpaweeklymission/global/GlobalExceptionHandler.java
@@ -3,36 +3,36 @@ package org.programmers.jpaweeklymission.global;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.Collections;
+import java.util.Map;
+
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private static final String MESSAGE = "message";
+
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public void handleEntityNotFoundException(EntityNotFoundException e) {
-        log.warn("EntityNotFoundException -> {}", e.getMessage(), e);
+        log.warn(e.getMessage(), e);
     }
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public void handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        log.warn("HttpMessageNotReadableException -> {}", e.getMessage(), e);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public void handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        log.warn("MethodArgumentNotValidException -> {}", e.getMessage(), e);
+    @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentNotValidException.class})
+    public ResponseEntity<Map<String, String>> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.warn(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(Collections.singletonMap(MESSAGE, e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public void handleException(Exception e) {
-        log.error("Exception -> {}", e.getMessage(), e);
+    public ResponseEntity<Map<String, String>> handleException(Exception e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.internalServerError().body(Collections.singletonMap(MESSAGE, e.getMessage()));
     }
 }
