a885e1e0018458f4c55ca5ac0656a3e0e6c9b02e
jun <alap_u@naver.com>
2023-08-01 23:25:59 +0900

    JPA Auditing 설정

---
+++ src/main/java/weekjpa/weekjpa/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package weekjpa.weekjpa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
