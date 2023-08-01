c41677c6fd45dad2a4fd5a7f441c85a1168042f1
seongHyun-Min <steve6133@naver.com>
2023-08-01 12:35:34 +0900

    init : 프로젝트 세팅

---
+++ src/main/java/com/example/springbootjpaweekly3/SpringbootJpaWeekly3Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootjpaweekly3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaWeekly3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaWeekly3Application.class, args);
+    }
+
+}
