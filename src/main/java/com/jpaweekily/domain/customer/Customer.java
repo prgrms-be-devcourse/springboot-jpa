2acc0c9045af1089f75ad29cc27b1c1684af34aa
weonest <geonhee33@naver.com>
2023-08-01 18:31:33 +0900

    feedback: Customer 객체 수정

--- src/main/java/com/jpaweekily/domain/customer/Customer.java
+++ src/main/java/com/jpaweekily/domain/customer/Customer.java
@@ -1,34 +1,35 @@
 package com.jpaweekily.domain.customer;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Table(name = "customers")
 @Getter
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
+    @Column(name = "first_name", nullable = false)
     private String lastName;
 
     @Builder
-    public Customer(String firstName, String lastName) {
+    private Customer(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public void changeFirstName(String firstName) {
+    public void update(String firstName, String lastName) {
         this.firstName = firstName;
-    }
-
-    public void changeLastName(String lastName) {
         this.lastName = lastName;
     }
 }
