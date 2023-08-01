19fabb1f44b9f4494526157735a3bce78f701b17
weonest <geonhee33@naver.com>
2023-08-01 22:18:41 +0900

    feedback: 사용하지 않는 메서드 삭제

--- src/main/java/com/jpaweekily/domain/customer/infrastructrue/CustomerRepository.java
+++ src/main/java/com/jpaweekily/domain/customer/infrastructrue/CustomerRepository.java
@@ -5,12 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-    Optional<Customer> findByFirstName(String firstname);
 
     Page<Customer> findAll(Pageable pageable);
 }
