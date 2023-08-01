30ff2da7f325d2e7c345a2e461a4295a06798cd6
bumwoo <gld55602@gmail.com>
2023-08-01 16:35:48 +0900

    test: 테스트 환경 수정

--- src/test/java/com/lecture/jpa/domain/order/OrderPersistenceTest.java
+++ src/test/java/com/lecture/jpa/domain/order/OrderPersistenceTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class OrderPersistenceTest {
 
     @Autowired
