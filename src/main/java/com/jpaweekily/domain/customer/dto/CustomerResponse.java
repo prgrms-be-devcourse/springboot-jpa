c4b15ec8155d407e7120792c534e067966c34d1d
weonest <geonhee33@naver.com>
2023-08-01 22:16:33 +0900

    feedback: CustomerResponse 변환 메서드 삭제

--- src/main/java/com/jpaweekily/domain/customer/dto/CustomerResponse.java
+++ src/main/java/com/jpaweekily/domain/customer/dto/CustomerResponse.java
@@ -1,13 +1,8 @@
 package com.jpaweekily.domain.customer.dto;
 
-import com.jpaweekily.domain.customer.Customer;
-
 public record CustomerResponse (
         Long id,
         String firstName,
         String lastName
 ){
-    public static CustomerResponse from(Customer customer) {
-        return new CustomerResponse(customer.getId(), customer.getFirstName(), customer.getLastName());
-    }
 }
