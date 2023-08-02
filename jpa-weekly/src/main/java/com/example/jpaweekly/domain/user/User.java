ef884239ef289fc7fd77729e1543b57b422c6e6c
na-yk <ninghao17@naver.com>
2023-08-02 16:53:10 +0900

    feat: User 엔티티 추가

---
+++ jpa-weekly/src/main/java/com/example/jpaweekly/domain/user/User.java
@@ -0,0 +1,36 @@
+package com.example.jpaweekly.domain.user;
+
+import com.example.jpaweekly.domain.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String nickname;
+
+    @Builder
+    public User(String loginId, String password, String nickname) {
+        this.loginId = loginId;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
