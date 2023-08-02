d84f0609d563dd60a55448e67370be0d48727b63
hseong3243 <hseong3243@gmail.com>
2023-08-03 00:54:54 +0900

    feat: Item 검증 추가

--- src/main/java/com/programmers/jpa/domain/Item.java
+++ src/main/java/com/programmers/jpa/domain/Item.java
@@ -5,31 +5,69 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Item {
+    private static final int ZERO = 0;
+    private static final int NAME_LENGTH = 200;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "item_id")
     private Long id;
 
-    @Column(nullable = false, length = 200)
+    @Column(nullable = false, length = NAME_LENGTH)
     private String name;
 
+    @Column(nullable = false)
     private int price;
+
+    @Column(nullable = false)
     private int stockQuantity;
 
     public Item(String name, int price, int stockQuantity) {
+        nullCheck(name);
         validateName(name);
+        validatePrice(price);
+        validateStockQuantity(stockQuantity);
         this.name = name;
         this.price = price;
         this.stockQuantity = stockQuantity;
     }
 
+    private void nullCheck(String name) {
+        if (Objects.isNull(name)) {
+            throw new IllegalArgumentException("Item name is mandatory");
+        }
+    }
+
     private void validateName(String name) {
-        if (name.length() > 200) {
+        if (name.length() > NAME_LENGTH) {
             throw new IllegalArgumentException("Invalid item name");
         }
+    }
+
+    private void validatePrice(int price) {
+        if(price < ZERO) {
+            throw new IllegalArgumentException("Price must be positive or zero");
+        }
+    }
+
+    private void validateStockQuantity(int stockQuantity) {
+        if(stockQuantity < ZERO) {
+            throw new IllegalArgumentException("Stock quantity must be positive or zero");
+        }
+    }
+
+    public void decreaseStockQuantity() {
+        validateStockQuantity(stockQuantity - 1);
+        stockQuantity--;
+    }
+
+    public void increaseStockQuantity() {
+        stockQuantity++;
     }
 }
