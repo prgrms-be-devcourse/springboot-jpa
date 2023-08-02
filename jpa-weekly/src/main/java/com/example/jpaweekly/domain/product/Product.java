d3177e20c8bdc8b99863c7cb87b24627bc865408
na-yk <ninghao17@naver.com>
2023-08-02 16:54:50 +0900

    feat: Product 엔티티 추가
    feat: Product 엔티티 추가

---
+++ jpa-weekly/src/main/java/com/example/jpaweekly/domain/product/Product.java
@@ -0,0 +1,29 @@
+package com.example.jpaweekly.domain.product;
+
+import com.example.jpaweekly.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String productName;
+
+    private int price;
+
+    @Builder
+    public Product(String productName, int price) {
+        this.productName = productName;
+        this.price = price;
+    }
+}
