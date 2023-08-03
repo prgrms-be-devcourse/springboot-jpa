94aa363183bc3417356505752f1f132ab50b5dd3
weonest <geonhee33@naver.com>
2023-08-03 20:59:34 +0900

    feedback: findAll 메서드 삭제 -> 이미 구현되어 있는 메서드임 (PagingAndSortingRepository)

--- src/main/java/com/jpaweekily/domain/customer/infrastructrue/CustomerRepository.java
+++ src/main/java/com/jpaweekily/domain/customer/infrastructrue/CustomerRepository.java
@@ -1,12 +1,8 @@
 package com.jpaweekily.domain.customer.infrastructrue;
 
 import com.jpaweekily.domain.customer.Customer;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-    Page<Customer> findAll(Pageable pageable);
 }
