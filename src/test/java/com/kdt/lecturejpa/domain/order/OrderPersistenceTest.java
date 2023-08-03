34ba995b81bad0e12d130aae17f0ea10c5511892
Curry4182 <kangbk4182@naver.com>
2023-08-04 00:55:03 +0900

    feat: 강의 중 연관관계 맵핑 테스트 코드 추가

--- src/test/java/com/kdt/lecturejpa/domain/order/OrderPersistenceTest.java
+++ src/test/java/com/kdt/lecturejpa/domain/order/OrderPersistenceTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.kdt.lecturejpa.domain.member.Member;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -57,7 +59,6 @@ public class OrderPersistenceTest {
 		Member memberEntity = entityManager.find(Member.class, 1L);
 
 		Order order = new Order();
-		order.setUuid(UUID.randomUUID().toString());
 		order.setOrderDateTime(LocalDateTime.now());
 		order.setOrderStatus(OPENED);
 		order.setMemo("부재시 전화주세요.");
@@ -66,10 +67,41 @@ public class OrderPersistenceTest {
 		entityManager.persist(order);
 		transaction.commit();
 
-		Order orderEntity = entityManager.find(Order.class, order.getUuid());
+		Order orderEntity = entityManager.find(Order.class, order.getId());
 		// FK 를 이용해 회원 다시 조회
 		Member orderMemberEntity = entityManager.find(Member.class, orderEntity.getMemberId());
 		// orderEntity.getMember() // 객체중심 설계라면 객체그래프 탐색을 해야하지 않을까?
 		log.info("nick : {}", orderMemberEntity.getNickName());
+	}
+
+	@Test
+	void 연관관계_테스트() {
+		EntityManager entityManager = emf.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+
+		transaction.begin();
+
+		Member member = new Member();
+		member.setName("byeonggon");
+		member.setNickName("guppy.kang");
+		member.setAddress("서울시 관악구");
+		member.setAge(33);
+
+		entityManager.persist(member);
+
+		Order order = new Order();
+		order.setOrderStatus(OPENED);
+		order.setOrderDateTime(LocalDateTime.now());
+		order.setMemo("부재시 연락주세요.");
+		order.setMember(member);
+		//member.setOrders(Lists.newArrayList(order)); //이 코드가 없어도 setMember를 수정해서 양방향으로 업데이트 되
+
+		entityManager.persist(order);
+		transaction.commit();
+		Order entity = entityManager.find(Order.class, order.getId());
+
+		log.info("{}", entity.getMember().getNickName()); // 객체 탐색 그래프
+		log.info("{}", entity.getMember().getOrders().size());
+		log.info("{}", order.getMember().getOrders().size());
 	}
 }
