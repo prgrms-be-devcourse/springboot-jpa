30c614885a03e2ee359107aaac1e23412a3272d1
Curry4182 <kangbk4182@naver.com>
2023-08-04 00:57:32 +0900

    feat: 연관 관계 맵핑을 이용한 주문 서비스 추가

---
+++ src/main/java/com/kdt/lecturejpa/domain/order/OrderService.java
@@ -0,0 +1,41 @@
+package com.kdt.lecturejpa.domain.order;
+
+import org.aspectj.weaver.ast.Or;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kdt.lecturejpa.domain.item.Item;
+import com.kdt.lecturejpa.domain.item.ItemRepository;
+import com.kdt.lecturejpa.domain.member.Member;
+import com.kdt.lecturejpa.domain.member.MemberRepository;
+import com.kdt.lecturejpa.domain.order_item.OrderItem;
+import com.kdt.lecturejpa.domain.order_item.OrderItemRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+	private final OrderRepository orderRepository;
+	private final MemberRepository memberRepository;
+	private final ItemRepository itemRepository;
+	private final OrderItemRepository orderItemRepository;
+	@Transactional
+	public Order createOrder(Long itemId, Long memberId, int orderItemQuantity) {
+		Item item = itemRepository.findById(itemId).orElseThrow(
+			() -> new RuntimeException("존재하지 않는 Item 입니다.")
+		);
+
+		Member member = memberRepository.findById(memberId).orElseThrow(
+			() -> new RuntimeException("존재하지 않는 member 입니다.")
+		);
+
+		OrderItem orderItem = OrderItem.createOrderItem(item, orderItemQuantity);
+
+		Order order = Order.createOrder(orderItem, member,"", OrderStatus.OPENED);
+		orderItem.setOrder(order);
+		orderRepository.save(order);
+		return order;
+	}
+}
