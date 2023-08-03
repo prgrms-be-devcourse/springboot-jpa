236d1cefdd7da0f4badd010ebaf9dd24561dea3c
sangminnim <sangmins930@gmail.com>
2023-08-03 15:07:42 +0900

    refactor: import 시 asterisk 제거하고 import 항목 명시

--- src/main/java/org/programmers/jpaweeklymission/customer/Customer.java
+++ src/main/java/org/programmers/jpaweeklymission/customer/Customer.java
@@ -1,6 +1,11 @@
 package org.programmers.jpaweeklymission.customer;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
