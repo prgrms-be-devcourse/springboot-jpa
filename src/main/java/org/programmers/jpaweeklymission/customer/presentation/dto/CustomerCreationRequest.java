d355a6bb4c4cef51b1e1fdbc4f028098190a03b7
sangminnim <sangmins930@gmail.com>
2023-08-03 19:57:26 +0900

    feat: message 추가

--- src/main/java/org/programmers/jpaweeklymission/customer/presentation/dto/CustomerCreationRequest.java
+++ src/main/java/org/programmers/jpaweeklymission/customer/presentation/dto/CustomerCreationRequest.java
@@ -1,14 +1,11 @@
 package org.programmers.jpaweeklymission.customer.presentation.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CustomerCreationRequest(
-        @NotBlank
-        @Size(min = 1, max = 20)
+        @Size(min = 1, max = 20, message = "First name must be at least 1 character and no more than 20 characters.")
         String firstName,
-        @NotBlank
-        @Size(min = 1, max = 20)
+        @Size(min = 1, max = 20, message = "Last name must be at least 1 character and no more than 20 characters.")
         String lastName
 ) {
 }
