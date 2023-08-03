5049134fd24da77c3832ca75099fdd7ae3d61785
박은지 <qkrdmswl1018@naver.com>
2023-08-03 22:48:02 +0900

    refactor: 자동으로 시간을 생성해 주도록 BaseEntity에 어노테이션 추가

--- src/main/java/com/programmers/week/base/BaseEntity.java
+++ src/main/java/com/programmers/week/base/BaseEntity.java
@@ -8,11 +8,14 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
   @Column(columnDefinition = "TIMESTAMP")
+  @CreatedDate
   private LocalDateTime createdAt;
 
   @Column(columnDefinition = "TIMESTAMP")
+  @LastModifiedDate
   private LocalDateTime lastModifiedAt;
 }
