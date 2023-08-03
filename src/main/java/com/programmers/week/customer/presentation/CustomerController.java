0dfc86b687f288b1a936de35b9ea904e2065e2f9
박은지 <qkrdmswl1018@naver.com>
2023-08-03 23:04:16 +0900

    refactor: RequestMapping 경로 수정

--- src/main/java/com/programmers/week/customer/presentation/CustomerController.java
+++ src/main/java/com/programmers/week/customer/presentation/CustomerController.java
@@ -1,43 +1,51 @@
-package com.programmers.week.customer.ui;
+package com.programmers.week.customer.presentation;
 
 import com.programmers.week.customer.application.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/customers")
 @RequiredArgsConstructor
 public class CustomerController {
 
-  private final CustomerService customerService;
+    private final CustomerService customerService;
 
-  @ResponseStatus(HttpStatus.CREATED)
-  @PostMapping("/customers")
-  public Long create(@RequestBody CustomerCreateRequest request) {
-    return customerService.create(request);
-  }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Long create(@RequestBody CustomerCreateRequest request) {
+        return customerService.create(request);
+    }
 
-  @ResponseStatus
-  @GetMapping("/customers/{id}")
-  public CustomerResponse findById(@PathVariable Long id) {
-    return customerService.findById(id);
-  }
+    @ResponseStatus
+    @GetMapping("/{id}")
+    public CustomerResponse findById(@PathVariable Long id) {
+        return customerService.findById(id);
+    }
 
-  @ResponseStatus
-  @GetMapping
-  public Page<CustomerResponse> findAll(PageRequestDto request) {
-    PageRequest pageRequest = PageRequest.of(request.page(), request.size());
-    return customerService.findAll(pageRequest);
-  }
+    @ResponseStatus
+    @GetMapping
+    public Page<CustomerResponse> findAll(@RequestBody PageRequestDto request) {
+        PageRequest pageRequest = PageRequest.of(request.page(), request.size());
+        return customerService.findAll(pageRequest);
+    }
 
-  @ResponseStatus
-  @PatchMapping("/customers/update")
-  public Long update(@RequestBody CustomerUpdateRequest request) {
-    return customerService.update(request);
-  }
+    @ResponseStatus
+    @PatchMapping("/{id}")
+    public Long update(@PathVariable Long id, @RequestBody CustomerUpdateRequest request) {
+        return customerService.update(id, request);
+    }
 
     @ResponseStatus
     @DeleteMapping("/customers/{id}")
