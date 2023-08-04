a9bb1c0cb46dbbc4b85e8cbdf9acced622b3b4a3
2jie0516 <2jie0516>
2023-08-04 21:51:26 +0900

    멤버 도메인 추가

---
+++ src/main/java/prgms/jpamission2/config/domain/Member.java
@@ -0,0 +1,45 @@
+package prgms.jpamission2.config.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "member")
+@Getter
+@Setter
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+
+    @Column(nullable = false, length = 30, unique = true)
+    private String nickName;
+
+    private int age;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "description", nullable = true)
+    private String description;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+    public void addOrder(prgms.mission3.order.domain.Order order) {
+        order.setMember(this);
+    }
+}
