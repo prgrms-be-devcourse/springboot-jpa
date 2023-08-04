dde5701387ac21d5c470a5ee2e1b1c12cf32eb75
2jie0516 <2jie0516>
2023-08-04 21:52:45 +0900

    아이템 도메인 추가

---
+++ src/main/java/prgms/jpamission2/config/domain/Furniture.java
@@ -0,0 +1,15 @@
+package prgms.jpamission2.config.domain;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@DiscriminatorValue("FURNITURE")
+public class Furniture extends Item{
+    private int width;
+    private int height;
+}
