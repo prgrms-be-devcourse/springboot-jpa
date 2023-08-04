67b6e00fa07e8125ff19d8296c3af2c7a67810ea
na-yk <ninghao17@naver.com>
2023-08-05 04:31:44 +0900

    feat: 기본 생성자 AccessLevel 설정

--- jpa-weekly/src/main/java/com/example/jpaweekly/domain/customer/Customer.java
+++ jpa-weekly/src/main/java/com/example/jpaweekly/domain/customer/Customer.java
@@ -5,13 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
