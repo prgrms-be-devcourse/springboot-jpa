a20c728ed22e5077a2ec5693537bfb58a2520c44
박은지 <qkrdmswl1018@naver.com>
2023-08-04 14:11:07 +0900

    refactor: BaseEntity 클래스를 추상 클래스로 변경

--- src/main/java/com/programmers/week/base/BaseEntity.java
+++ src/main/java/com/programmers/week/base/BaseEntity.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseEntity {
+public abstract class BaseEntity {
 
     @Column(columnDefinition = "TIMESTAMP")
     @CreatedDate
