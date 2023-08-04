4e0e8fba2ed425f722f1909b49abe5f2f5db6279
2jie0516 <2jie0516>
2023-08-04 21:52:08 +0900

    베이스 엔티티 설정

---
+++ src/main/java/prgms/jpamission2/config/domain/BaseEntity.java
@@ -0,0 +1,18 @@
+package prgms.jpamission2.config.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+    @Column(name = "created_by")
+    private String createdBy;
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime cratedAt;
+}
