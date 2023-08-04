f58be7a60f04775c998048c76092962496859131
na-yk <ninghao17@naver.com>
2023-08-04 13:40:26 +0900

    feat: lastName에 @Column 스키마 표현

--- jpa-weekly/src/main/java/com/example/jpaweekly/domain/customer/Customer.java
+++ jpa-weekly/src/main/java/com/example/jpaweekly/domain/customer/Customer.java
@@ -16,6 +16,8 @@ public class Customer {
 
     @Column(name="first_name",nullable = false, unique = false)
     private String firstName;
+
+    @Column(name="last_name", nullable = false)
     private String lastName;
 
     @Builder
