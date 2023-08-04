979d45116933e0cdda06ce75a3d4ede5f9db7721
na-yk <ninghao17@naver.com>
2023-08-05 05:16:43 +0900

    feat: @NoArgsConstructor 사용

--- jpa-weekly/src/main/java/com/example/jpaweekly/domain/customer/CustomerMapper.java
+++ jpa-weekly/src/main/java/com/example/jpaweekly/domain/customer/CustomerMapper.java
@@ -2,11 +2,10 @@ package com.example.jpaweekly.domain.customer;
 
 import com.example.jpaweekly.domain.customer.dto.CustomerRequest;
 import com.example.jpaweekly.domain.customer.dto.CustomerResponse;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 public class CustomerMapper {
-
-    private CustomerMapper(){
-    }
 
     public static CustomerResponse convertEntityToResponse(Customer customer){
         return new CustomerResponse(customer.getId(), customer.getFirstName(), customer.getLastName());
