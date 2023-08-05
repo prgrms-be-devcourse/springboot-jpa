e4853c3c61976d83fbead7a5167dfb5a4fc70225
sangminnim <sangmins930@gmail.com>
2023-08-05 12:22:30 +0900

    feat: 에러메세지 수정

--- src/main/java/org/programmers/jpaweeklymission/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/programmers/jpaweeklymission/global/exception/GlobalExceptionHandler.java
@@ -23,7 +23,11 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         log.warn(e.getMessage(), e);
-        return ErrorResponse.newErrorResponse(e.getMessage());
+        // TODO:
+        //  이 에러 매세지를 출력하는 방법이 여러 방법 있을거 같은데 어떻게 하시나요?
+        //  저는 일단 이게 코드로 짧고 가독성도 좋아서 이렇게 했는데, ExceptionHandler 라서 null 체크 안 해줘도 되는데 intellij 노란줄이 계속 거슬립니다.
+        //  그리고 굳이 에러 메세지를 자세하게 해줘야 되나 생각이 듭니다. 아래 HttpMessageNotReadableException 와 묶어서 에러 메세지를 동일한 "Invalid Request." 로 하는 거 어떤가요?
+        return ErrorResponse.newErrorResponse(e.getFieldError().getDefaultMessage());
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
