562cd04045accf9d6cd47a0c72981db698e7f897
na-yk <ninghao17@naver.com>
2023-08-06 20:44:29 +0900

    style: 불필요한 import 제거
    style: 불필요한 import 제거

--- jpa-weekly/src/main/java/com/example/jpaweekly/domain/user/dto/UserCreateRequest.java
+++ jpa-weekly/src/main/java/com/example/jpaweekly/domain/user/dto/UserCreateRequest.java
@@ -1,7 +1,6 @@
 package com.example.jpaweekly.domain.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record UserCreateRequest(
         @NotBlank
