a2660f50a18a4855876e6e9bf294e2af14ca086b
sangminnim <sangmins930@gmail.com>
2023-08-06 18:43:58 +0900

    feat: import asterisk 제거

--- src/main/java/org/programmers/jpaweeklymission/item/domain/Item.java
+++ src/main/java/org/programmers/jpaweeklymission/item/domain/Item.java
@@ -1,6 +1,10 @@
 package org.programmers.jpaweeklymission.item.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
