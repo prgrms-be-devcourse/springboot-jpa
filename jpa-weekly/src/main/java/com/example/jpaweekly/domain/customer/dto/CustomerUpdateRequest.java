79d2fe335c198174a7edb173301e80ad628f3015
na-yk <ninghao17@naver.com>
2023-08-06 20:43:35 +0900

    refactor: Request dto validation 추가

--- jpa-weekly/src/main/java/com/example/jpaweekly/domain/customer/dto/CustomerUpdateRequest.java
+++ jpa-weekly/src/main/java/com/example/jpaweekly/domain/customer/dto/CustomerUpdateRequest.java
@@ -1,8 +1,14 @@
 package com.example.jpaweekly.domain.customer.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record CustomerUpdateRequest(
+        @NotNull
         Long id,
+        @NotBlank
         String firstName,
+        @NotBlank
         String lastName
 ) {
 }
--- jpa-weekly/src/main/java/com/example/jpaweekly/domain/user/dto/UserCreateRequest.java
+++ jpa-weekly/src/main/java/com/example/jpaweekly/domain/user/dto/UserCreateRequest.java
@@ -1,8 +1,14 @@
 package com.example.jpaweekly.domain.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record UserCreateRequest(
+        @NotBlank
         String loginId,
+        @NotBlank
         String password,
+        @NotBlank
         String nickname
 ) {
 }
