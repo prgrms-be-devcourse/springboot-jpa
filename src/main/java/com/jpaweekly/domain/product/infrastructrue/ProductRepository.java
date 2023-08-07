5f5d625cd1711202d6b0dfc2ab746c1ab1b779d7
weonest <geonhee33@naver.com>
2023-08-07 14:59:20 +0900

    feat: Product 서비스 레포지 기능 추가

--- src/main/java/com/jpaweekly/domain/product/infrastructrue/ProductRepository.java
+++ src/main/java/com/jpaweekly/domain/product/infrastructrue/ProductRepository.java
@@ -3,6 +3,9 @@ package com.jpaweekly.domain.product.infrastructrue;
 import com.jpaweekly.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    Optional<Product> findByProductName(String productName);
 }
