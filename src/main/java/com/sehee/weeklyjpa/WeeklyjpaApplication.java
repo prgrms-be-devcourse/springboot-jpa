c77990e2e87d8ad7344b53394d1c3de85462880e
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-13 19:13:16 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/sehee/weeklyjpa/WeeklyjpaApplication.java
@@ -0,0 +1,13 @@
+package com.sehee.weeklyjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeeklyjpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sehee/weeklyjpa/WeeklyjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sehee.weeklyjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
