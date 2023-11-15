d71d041f1181e53dd3b3ed621803df3e3a82d5ac
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-15 18:58:37 +0900

    feat: BaseEntity getter, setter 적용

--- src/main/java/com/sehee/weeklyjpa/domain/BaseEntity.java
+++ src/main/java/com/sehee/weeklyjpa/domain/BaseEntity.java
@@ -2,9 +2,13 @@ package com.sehee.weeklyjpa.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @MappedSuperclass
 public class BaseEntity {
     @Column(name = "created_by")
