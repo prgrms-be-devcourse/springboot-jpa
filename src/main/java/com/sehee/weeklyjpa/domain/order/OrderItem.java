664cc349010cbb082fe0bb62b024c6c481af2022
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-15 17:29:40 +0900

    feat: 주문에 필요한 클래스 정의

---
+++ src/main/java/com/sehee/weeklyjpa/domain/order/OrderItem.java
@@ -0,0 +1,45 @@
+package com.sehee.weeklyjpa.domain.order;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private int price;
+    private int quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", referencedColumnName = "id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", referencedColumnName = "id")
+    private Item item;
+
+    public void setOrder(Order order) {
+        if (Objects.nonNull(this.order)) {
+            this.order.getOrderItems().remove(this);
+        }
+        this.order = order;
+        order.getOrderItems().add(this);
+    }
+
+    public void setItem(Item item) {
+        if (Objects.nonNull(this.item)) {
+            this.item.getOrderItems().remove(this);
+        }
+        this.item = item;
+        item.getOrderItems().add(this);
+    }
+}
