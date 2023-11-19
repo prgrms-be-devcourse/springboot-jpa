b99e7b842abdfe911bdc048aa23bdc8167a2bac3
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-19 23:13:19 +0900

    faet: sql 문 수정

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,5 +1,27 @@
-CREATE DATABASE IF NOT EXISTS test;
+CREATE
+DATABASE IF NOT EXISTS test;
 
 USE test;
 
+create table customers
+(
+    id bigint primary key ,
+    username varchar(20),
+    name varchar(20),
+    password varchar(50),
+    nickname varchar(20)
+);
 
+
+CREATE
+DATABASE IF NOT EXISTS test_code;
+USE test_code;
+
+create table customers
+(
+    id bigint primary key ,
+    username varchar(20),
+    name varchar(20),
+    password varchar(50),
+    nickname varchar(20)
+);
