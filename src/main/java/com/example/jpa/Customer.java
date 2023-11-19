fb7d84c2064b87a753523c2d88b06904ed52b70e
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-19 23:13:40 +0900

    faet: table 명 수정

--- src/main/java/com/example/jpa/Customer.java
+++ src/main/java/com/example/jpa/Customer.java
@@ -1,9 +1,6 @@
 package com.example.jpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "customers")
 public class Customer {
 
     @Id
