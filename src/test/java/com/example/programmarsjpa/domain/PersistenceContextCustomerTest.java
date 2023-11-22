001d979160726f547b32f82e656971506b22de60
voidmelody <sss9073@naver.com>
2023-11-22 19:56:04 +0900

    feat: "2번 미션 수행(영속성 컨텍스트)"

--- src/test/java/com/example/programmarsjpa/domain/PersistenceContextCustomerTest.java
+++ src/test/java/com/example/programmarsjpa/domain/PersistenceContextCustomerTest.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class PersistenceContextCustomerTest {
+class PersistenceContextCustomerTest {
 
     @Autowired
     CustomerRepository repository;
